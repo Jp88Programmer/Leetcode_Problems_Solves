ca9c3d3de51ab4c84c9f38f4ae3f58796a231287
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-19 14:22:25 +0530

    Time: 104 ms (77.16%), Space: 46.7 MB (38.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    
+    if(nums[0] == 0 && nums.length > 1) return false;
+    
+    let i = 0;
+    let reach = 0;
+    for(i = 0; i<nums.length && i <= reach ; i++){
+        reach = Math.max(reach,i+nums[i]);
+    }
+    
+    return i === nums.length;
+};
