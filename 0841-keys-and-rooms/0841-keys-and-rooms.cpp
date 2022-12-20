df95037e4676587fa4a394feaa8e2f0ea17c5417
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-20 10:23:07 +0530

    Time: 18 ms (56.98%), Space: 11.2 MB (11.50%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -10,7 +10,7 @@ public:
                 dfs(gr,visited,adj);
         }
     }
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+    bool canVisitAllRooms(vvi& rooms) {
         
         int n = rooms.size();
         vvi gr(n);
