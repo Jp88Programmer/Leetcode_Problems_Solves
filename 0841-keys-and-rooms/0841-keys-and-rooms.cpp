86b21cf94cd61a430a70abd86cf10258294e15b2
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-20 10:25:05 +0530

    Time: 23 ms (34.07%), Space: 11.2 MB (11.50%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -3,30 +3,23 @@
 class Solution {
 public:
     void dfs(vvi &gr,vi&visited,int id){
-        
         visited[id] = 1;
-        for(auto adj : gr[id]){
-            if(!visited[adj])
-                dfs(gr,visited,adj);
-        }
+        for(auto adj : gr[id])
+            if(!visited[adj]) dfs(gr,visited,adj);
     }
+    
     bool canVisitAllRooms(vvi& rooms) {
         
         int n = rooms.size();
         vvi gr(n);
-        
-        for(int i = 0; i < n ; i++){
-            gr[i] = rooms[i];
-        }
+        for(int i = 0; i < n ; i++) gr[i] = rooms[i];
         
         vi visited(n,0);
         dfs(gr,visited,0);
         
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<n; i++)
             if(!visited[i]) return false;
-        }
         
         return true;
-        
     }
 };
