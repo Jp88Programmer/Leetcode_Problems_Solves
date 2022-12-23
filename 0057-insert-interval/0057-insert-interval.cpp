d71a9a9084c5f0dd7768405149c50f407e5ccc86
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-23 13:41:15 +0530

    Time: 28 ms (65.89%), Space: 17 MB (91.96%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,6 +4,7 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
+        /*
         int n = intervals.size();
         vi start,end;
         
@@ -25,56 +26,29 @@ public:
         
         result.push_back(newInterval);
         return result ;
-        /*
-        for(int i = 0;  i<n; i++){
-            start.push_back(intervals[i][0]);
-            start.push_back(intervals[i][1]);
-        }
-        
-        sort(start.begin(),start.end());
-        
-        // for(auto i : start) cout << i << " ";
-        
-        int s = newInterval[0];
-        int e = newInterval[1];
-        
-        for(int i = 0; i<start.size() ; i++){
-            
-            if(i!=0 && start[i] > s){
-                s = start[i-1];
-                break;
-            }
-        }
-        
-        for(int i = 0; i<start.size() ; i++){
-            if(start[i] > e){
-                e = start[i];
-                // cout << e << endl;
-                break;
-            }
-        }
-        
-        cout << s << " " << e << endl;
-        vii ans ;
-        for(int i = 0; i < n ; i++){
-            
-            if(intervals[i][0] < s){
-                
-                if(intervals[i][1] < s && intervals[i][1] < e)
-                    ans.push_back(intervals[i]);
-                
-            }else{
-                
-                if(intervals[i][1] > s && intervals[i][1] > e)
-                    ans.push_back(intervals[i]);
-            }
-        }
-        
-        ans.push_back({s,e});
-        
-        sort(ans.begin(),ans.end());
-        
-        return ans;
         */
+        
+        int n = intervals.size();
+        
+        int pos = lower_bound(begin(intervals), end(intervals), newInterval) - begin(intervals);
+        
+        intervals.insert(intervals.begin() + pos, newInterval);
+        
+        int j = 0;
+        for(int i = 1;i <= n;i++){
+            if(intervals[i][0] <= intervals[j][1])
+            {
+                intervals[j][0] = min(intervals[j][0], intervals[i][0]);
+                intervals[j][1] = max(intervals[j][1], intervals[i][1]);
+            }
+            else{
+                intervals[++j] = intervals[i];
+            }
+                
+        }
+        intervals.resize(j + 1);
+        return intervals;
+        
+        
     }
 };
