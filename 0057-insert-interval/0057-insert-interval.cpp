1ea0a8bed8f93739d4aa939305225f7522fe39ab
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-23 13:43:18 +0530

    Time: 19 ms (85.84%), Space: 17.1 MB (39.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,7 +4,8 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        /*
+        //solution 1 
+        
         int n = intervals.size();
         vi start,end;
         
@@ -12,12 +13,17 @@ public:
         
         for(int i = 0; i<n; i++){
             
+            //the new interval is after the range of other interval, so we can leave the current interval baecause the new one does not overlap with it
             if(intervals[i][1] < newInterval[0])
                 result.push_back(intervals[i]);
+            
+            // the new interval's range is before the other, so we can add the new interval and update it to the current one
             else if(intervals[i][0] > newInterval[1]){
                 result.push_back(newInterval);
                 newInterval = intervals[i];
             }
+            
+            // the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval 
             else if(intervals[i][0] <= newInterval[1] || intervals[i][1] >= newInterval[0]){
                 newInterval[0] = min(newInterval[0],intervals[i][0]);
                 newInterval[1] = max(newInterval[1],intervals[i][1]);
@@ -26,8 +32,10 @@ public:
         
         result.push_back(newInterval);
         return result ;
-        */
         
+        
+        /*
+        //solution 2 
         int n = intervals.size();
         
         int pos = lower_bound(begin(intervals), end(intervals), newInterval) - begin(intervals);
@@ -48,7 +56,7 @@ public:
         }
         intervals.resize(j + 1);
         return intervals;
-        
+        */
         
     }
 };
