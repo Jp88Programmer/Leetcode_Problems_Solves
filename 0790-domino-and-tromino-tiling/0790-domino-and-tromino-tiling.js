ac050bed6ecc7690159ec971d1cd70f6843f40d0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-24 12:48:37 +0530

    Time: 73 ms (79.17%), Space: 43.9 MB (58.33%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTilings = function(n) {
+    
+    let mod = 1e9+7;
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+    
+    let arr = [1,1,2];
+    
+    for(let i = 3; i<=n ; i++){
+        
+        let val = (2*arr[i-1] + arr[i-3])%mod;
+        arr.push(val);
+    }
+    
+    return arr[n];
+};
