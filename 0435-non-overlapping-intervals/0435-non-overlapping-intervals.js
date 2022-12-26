876eac8377f68a2ef91ffcfe9a8b115ea3c015cb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-26 12:21:43 +0530

    Time: 274 ms (84.94%), Space: 73.4 MB (94.60%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    
+    intervals.sort((a,b)=> a[1] - b[1]);
+    
+    console.log(intervals);
+    
+    let pre = 0,count = 0;
+    for(let i = 1; i < intervals.length; i++){
+        
+        if(intervals[i][0] < intervals[pre][1])
+            count++;
+        else
+            pre = i;
+    }
+    
+    return count;
+};
