307848f0ac3840ecfb88af2c76ae9da3b6f3f2a7
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2022-12-27 11:18:27 +0530

    Time: 102 ms (83.22%), Space: 45.7 MB (15.88%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    
+    // sort this method to sort all the type or also make the user
+    //defined sort 
+    g.sort((a,b) => a-b );
+    s.sort((a,b)=> a - b);
+    
+    console.log(g,s);
+    
+    let n = g.length,m = s.length;
+    let i=0;
+
+    for(let j = 0;  i<n && j < m ; j++){
+        
+        if(s[j] >= g[i]) i++;
+    }
+    
+    
+    return i;
+};
