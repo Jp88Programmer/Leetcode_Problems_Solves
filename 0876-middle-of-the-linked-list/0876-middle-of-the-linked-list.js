4d801e406ef8e6a412375e5e160bab3f7ec1aeea
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-01 12:00:12 +0530

    Time: 71 ms (70.83%), Space: 41.9 MB (73.68%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    
+    let first = head, second = head;
+    while(first && first.next){
+        first = first.next.next;
+        second = second.next;
+    }
+    return second;
+};
