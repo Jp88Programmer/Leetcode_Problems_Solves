8faa56e2e7d5c59ce791c6bee9c8b62aab3a2ca4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-03 10:59:46 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -22,9 +22,9 @@ class Solution
     void merge(int arr[], int l, int m, int r)
     {
          // Your code here
-        //  vector<int>temp;
+         vector<int>temp;
         // int temp[r]
-        int temp[100000];
+        // int temp[100000];
         int i = l;
         int f = l;
         int j = m+1;
@@ -32,35 +32,35 @@ class Solution
         while(i <= m && j <= r){
             
             if(arr[i] < arr[j]){
-                // temp.push_back(arr[i++]);
-                temp[f] = arr[i++];
+                temp.push_back(arr[i++]);
+                // temp[f] = arr[i++];
             }
             else
-                // temp.push_back(arr[j++]);
-                temp[f] = arr[j++];
-            f++;
+                temp.push_back(arr[j++]);
+                // temp[f] = arr[j++];
+            // f++;
         }
         
         if(i > m){
             while(j <= r){
-            // temp.push_back(arr[j++]);
-                temp[f++] = arr[j++];
+            temp.push_back(arr[j++]);
+                // temp[f++] = arr[j++];
             }
         }else{
             while(i <= m){
-                // temp.push_back(arr[i++]);
-                temp[f++] = arr[i++];
+                temp.push_back(arr[i++]);
+                // temp[f++] = arr[i++];
             }   
         }
         
         
-        // for(int i = l,k = 0 ; i <= r && k < temp.size() ; i++){
-        //     arr[i] = temp[k++];
-        // }
-         
-         for (int f = l ; f <= r; f++) {
-            arr[f] = temp[f] ;
+        for(int i = l,k = 0 ; i <= r && k < temp.size() ; i++){
+            arr[i] = temp[k++];
         }
+         
+        //  for (int f = l ; f <= r; f++) {
+        //     arr[f] = temp[f] ;
+        // }
     }
     public:
     void mergeSort(int arr[], int l, int r)
