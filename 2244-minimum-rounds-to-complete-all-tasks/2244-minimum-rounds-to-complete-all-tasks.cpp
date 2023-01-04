ad4cbb5ec2f4068af13b9a327cee1be1ef106471
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-04 10:28:00 +0530

    Time: 589 ms (26.35%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -12,9 +12,13 @@ public:
             
             if(x.second == 1) return -1;
             
+            /*
             if(x.second % 3 == 0) count += x.second / 3;
             if(x.second % 3 == 1) count+= x.second/3 - 1 + 2;
             if(x.second % 3 == 2) count += x.second / 3 + 1;
+            */
+            // count += ceil(x.second/3);
+            count += (x.second + 2) / 3;
         }
         
         return count;
