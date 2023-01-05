e228c3c2ef81e020cd4be20a5f67ec3a9978cc43
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-05 17:33:57 +0530

    Time: 351 ms (60.69%), Space: 65.7 MB (97.93%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -4,13 +4,15 @@
  */
 var findMinArrowShots = function(points) {
     
-    points.sort((a,b) => a[0] - b[0]);
+    points.sort((a,b) => a[1] - b[1]);
     
     console.log(points);
     let count = 1;
-    let s = points[0][0],e = points[0][1];
+    // let s = points[0][0],e = points[0][1];
+    let pos = points[0][1];
     for(let i = 1; i<points.length; i++){
         
+        /*
         if(points[i][0] <= e && points[i][1] >= s){
             s = Math.min(s,points[i][0]);
             e = Math.min(e,points[i][1]);
@@ -20,6 +22,12 @@ var findMinArrowShots = function(points) {
             e = points[i][1];
             
         }
+        */
+        
+        if(pos >= points[i][0])
+            continue;
+        count++;
+        pos = points[i][1];
     }
     
     return count;
