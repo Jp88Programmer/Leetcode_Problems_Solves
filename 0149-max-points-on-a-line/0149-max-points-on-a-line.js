8b05f7f2c1376b85423ae4a2a2f29461427edfe4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-08 23:54:20 +0530

    Time: 212 ms (47.82%), Space: 55.5 MB (20.87%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.js
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -8,18 +8,19 @@ var maxPoints = function(p) {
     let ans  = 2;
     for(let i = 0; i<p.length; i++){
         
-        let fr = 1; 
-        // let m = new Map();
+        //using map 
         const m = {};
         for(let j = 0; j < p.length; j++){
             
             if(i === j) continue;
+            
+            //to put the slope 
             let key = Math.atan2(p[j][1] - p[i][1],p[j][0] - p[i][0]);
             
             m[key] = (m[key] || 1) + 1;
             
         }
-        // ans = Math.max(ans,);
+        
         ans = Math.max(ans,...Object.values(m));
     }
     
