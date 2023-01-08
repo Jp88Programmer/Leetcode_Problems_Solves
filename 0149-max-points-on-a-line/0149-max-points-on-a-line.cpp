3ce7bc012a885d4007c1caa4629b39717766933f
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-08 23:28:30 +0530

    Time: 28 ms (95.00%), Space: 12.2 MB (73.44%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -14,8 +14,6 @@ public:
     }
     int maxPoints(vector<vector<int>>& p) {
         
-        // unordered_map<double,pair<int,unordered_map<int,int>>>m;
-        
         if(p.size() <= 2) return p.size();
         int ans = 2;
         
@@ -27,58 +25,9 @@ public:
                 
                 float s = calslope(p[j][0],p[j][1],p[i][0],p[i][1]);
                 
-//                 if(s == INT_MAX){
-                    
-//                 }else{
-                    
-//                 }
-                
                 m[s]++;
                 
                 fr = max(fr,m[s]);
-                
-                /*
-                // cout << s << endl;
-                if(div0){
-                    div0 = false;
-                    inx++;
-                    continue;
-                }
-                
-                // unordered_map<int,int>vis = m[s].second;
-                if(m.find(s) == m.end()){
-                    
-                    m[s].first = 2;
-                    m[s].second[p[j][0]] = p[j][1];
-                    m[s].second[p[i][0]] = p[i][1];
-                }else{
-                    pair<int,unordered_map<int,int>>p1 = m[s];
-                    // m[s].first\
-                    cout << m[s].first << endl;
-                // if(vis.find(p[j][0]) != vis.end() && vis[p[j][0]] == p[j][1]){
-                
-                if(p1.second.find(p[j][0]) != p1.second.end() && p1.second[p[j][0]] != p1.second[p[j][1]]){
-                    
-                    m[s].first++;
-                    // m[s].second[p[j][0]] = p[j][1];
-                    // m[s].second[p[j][0]] = p[j][1];
-                }
-                // else if(p1.second.find(p[j][0]) != p1.second.end() && p1.second[p[j][0]] != p1.second[p[j][1]]){
-                //     m[s].first++;
-                //     // m[s].second[p[j][0]] = p[j][1];
-                // }
-                else if(p1.second.find(p[j][0]) == p1.second.end()){
-                    // m[s].first = 2;
-                    m[s].first++;
-                    m[s].second[p[j][0]] = p[j][1];
-                    m[s].second[p[i][0]] = p[i][1];
-                    // m[s].second[p[j][0]] = p[j][1];
-                    // m[s][p[j][0]] = p[j][1];
-                    // m[s][p[i][0]] = p[i][1];
-                }
-                }
-                
-                */
             }
             
             ans = max(ans,fr+1);
