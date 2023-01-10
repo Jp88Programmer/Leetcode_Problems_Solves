dbb57aec85e5bc97d50d5cdd5cd45dbe63adc52f
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-10 22:41:55 +0530

    Time: 0 ms (100.00%), Space: 10 MB (48.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,10 +14,9 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
         if(!p && !q) return true;
-        if(p && !q || !p && q) return false;
-        if(p->val != q->val) return false;
+        if(p && !q || !p && q || p->val != q->val) return false;
+        // if(p->val != q->val) return false;
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        
-
+    
     }
 };
