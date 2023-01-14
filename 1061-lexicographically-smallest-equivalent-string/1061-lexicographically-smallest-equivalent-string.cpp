37047d834f744f42f19fa3fbf869256662ff1db0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-14 23:26:05 +0530

    Time: 9 ms (22.02%), Space: 7 MB (11.01%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     
+    //to find the both character in the list or not
     int found(vector<set<char>>&vset,char ch1,char ch2){
         
         for(int i = 0; i<vset.size(); i++){
-            
             if(vset[i].find(ch1) != vset[i].end() || vset[i].find(ch2) != vset[i].end()){
                 return i;
             }
@@ -12,6 +12,8 @@ public:
         
         return -1;
     }
+    
+    //only one character in list or not 
     int found(vector<set<char>>&vset,char ch){
         
         for(int i = 0; i<vset.size(); i++){
@@ -24,12 +26,12 @@ public:
         return -1;
     }
     
+    //when the many same character are different index set's element therfore
+    //to merge them 
     void mergearound(vector<set<char>>&vset){
         
         for(int i =0; i < vset.size();i++){
-            
             for(int j = i+1; j < vset.size();j++){
-                
                 for(auto sval : vset[j]){
                     if(vset[i].find(sval) != vset[i].end()){
                         vset[i].insert(vset[j].begin(),vset[j].end());
@@ -43,10 +45,12 @@ public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         
         
+        //list/array of the set
         vector<set<char>>vset;
         
         for(int i = 0; i<s1.size(); i++){
             
+            //find if yes return the index
             int id = found(vset,s1[i],s2[i]);
             
             if(id!=-1){
@@ -60,6 +64,7 @@ public:
             }
         }
         
+        //merge the list 
         mergearound(vset);
         string ans = "";
         for(int i =0; i<baseStr.size(); i++){
