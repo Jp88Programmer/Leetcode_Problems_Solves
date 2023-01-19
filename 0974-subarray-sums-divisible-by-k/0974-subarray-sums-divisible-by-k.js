5ab890c5f8b055f0a56cd9db5dc6cf8658420186
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-19 23:26:00 +0530

    Time: 117 ms (40.32%), Space: 47.7 MB (20.16%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function(nums, k) {
+    
+    let map = [0];
+    map[0] = 1;
+    let sum = 0,count = 0;
+    nums.forEach((e)=>{
+        sum += e ;
+        let r = (sum % k + k)%k;
+        
+        map[r] = map[r] || 0;
+        count += map[r]
+        map[r]++;
+    })
+    console.log(map);
+    return count;
+};
