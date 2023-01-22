6adc7b25cc4b6c865d4902e79b9e1de4840b3015
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-22 10:53:33 +0530

    Time: 163 ms (50.00%), Space: 72.4 MB (30.00%) - LeetHub

---
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+        long long d=0,u=0;
+        
+        for(int i = 0; i<nums1.size(); i++){
+            if(nums1[i] == nums2[i]) continue;
+            
+            int t = abs(nums1[i]-nums2[i]);
+            
+            if(k == 0) return -1;
+            else if(t %k == 0){
+                if(nums1[i] > nums2[i])
+                    d += t/k;
+                else
+                    u += t/k;
+            }
+            else
+                return -1;
+            
+            
+        }
+        
+        return d == u ? u : -1;
+    }
+};
