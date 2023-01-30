88e8078edb787ae1f1282ff31947a57d6b4e4120
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-01-30 21:41:53 +0530

    Time: 61 ms (82.04%), Space: 42 MB (24.68%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-    
-        // int tib[n+1];
-        let tib = Array(n+1);
+    // nst tribonacci = (n) =>{
+    if(n <= 1) return n;     
+    let prev = [1, 1, 0];
         
-        if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
-        
-        tib[0] = 0;
-        tib[1] = 1;
-        tib[2] = 1;
-        for(let i = 3; i<=n ; i++){
-            tib[i] = tib[i-1] + tib[i-2] + tib[i-3];
-        }
-        
-        return tib[n];
+    for(let i = 3; i <= n; i++) {
+        let curr = prev[0] + prev[1] + prev[2];
+        prev[2] = prev[1];
+        prev[1] = prev[0];
+        prev[0] = curr;
+    }
+    return prev[0];
 };
