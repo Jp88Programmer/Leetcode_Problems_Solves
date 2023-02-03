9ed272167a4cef4fb854499f280b664c491c0527
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-03 21:55:17 +0530

    Time: 55 ms (23.25%), Space: 10.8 MB (41.51%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string convert(string str, int r) {
+        
+        // string ans[r] = {""};
+        if(r == 1) return str;
+        vector<string>ans(r,"");
+        bool neg = false;
+        for(int i = 0,j = 0; i<str.size(); i++){
+            
+            if(!neg){
+                ans[j] += str[i];
+                j++;
+            }else{
+                ans[j] += str[i];
+                j--;
+            }            
+            
+            if(j == r-1 || j == 0){
+                neg = !neg;
+            }
+            
+            // if(j >= r-1){
+            //     j--;
+            // }
+            
+        }
+        
+        string res;
+        for(int i = 0; i<r; i++){
+            cout << ans[i] << endl;
+            res += ans[i];
+        }
+        
+        return res;
+    }
+};
