a55fe1b4c0321928020c007a5aafcf3c4500c8a0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-04 22:50:39 +0530

    Time: 4007 ms (100.00%), Space: 52.1 MB (100.00%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.js
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.js
@@ -1,22 +1,40 @@
-class Solution {
-public:
-    int maxCount(vector<int>& banned, int n, int maxSum) {
-        
-        unordered_map<int,int>m;
-        for(auto i : banned){
-            m[i]++;
-        }
-        
-        int sum = 0,count = 0;
-        for(int i = 1; i <=n; i++){
-            
-            if(m[i] == 0){
-                sum += i;
-                if(sum <= maxSum)
-                    count++;
-            }
-        }
-        
-        return count;
+/**
+ * @param {number[]} banned
+ * @param {number} n
+ * @param {number} maxSum
+ * @return {number}
+ */
+var maxCount = function(banned, n, maxSum) {
+    
+    /*
+    let m = new Map();
+    
+    for(let i = 0; i<banned.length; i++){
+        if(m.get(banned[i]) === undefined)
+            m.set(1);
+        else
+            m.set(m.get(banned[i])+1);
     }
+    
+    let sum = 0,count = 0;
+    for(let i = 1 ; i<=n ; i++){
+        if(!m.has(i)){
+            sum += i;
+            if(sum <= maxSum)
+                count++;
+            
+        }
+    }
+    
+    return count;
+    */
+    let sum = 0,count = 0;
+    
+    for(let i = 1 ;i<=n; i++){
+        if(!banned.includes(i) && sum + i <= maxSum){
+            sum += i;
+            count++;
+        }
+    }
+    return count;
 };
