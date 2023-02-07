e58e20b1611198756d8a031ffcc86988c8404749
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-07 22:47:07 +0530

    Time: 67 ms (76.07%), Space: 41.9 MB (79.29%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -6,18 +6,19 @@
 var arithmeticTriplets = function(nums, diff) {
     
     let count = 0;
+    
+    const map = new Map();
+    
     for(let i = 0; i<nums.length; i++){
-        
-        for(let j = i+1; j < nums.length; j++){
-            
-            for(let k = j+1 ; k < nums.length; k++){
-                
-                if(nums[j]-nums[i] === diff && nums[k]-nums[j] === diff){
-                    count++;
-                }
-            }
-        }
+        map.set(nums[i],true);
     }
     
+    
+    for(let i = 0; i<nums.length; i++){
+        if(map.has(nums[i]-diff) && map.has(nums[i]+diff))
+            count++;
+    }
+        
+      
     return count;
 };
