9afce93c6abf07b000562a15ec38254a4120b7c0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-07 22:39:35 +0530

    Time: 95 ms (34.82%), Space: 41.6 MB (95.36%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    
+    let count = 0;
+    for(let i = 0; i<nums.length; i++){
+        
+        for(let j = i+1; j < nums.length; j++){
+            
+            for(let k = j+1 ; k < nums.length; k++){
+                
+                if(nums[j]-nums[i] === diff && nums[k]-nums[j] === diff){
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+};
