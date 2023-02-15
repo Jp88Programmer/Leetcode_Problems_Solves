2451a6495dcaca1d4edd75d3e0272c798cddd514
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-15 21:56:43 +0530

    Time: 115 ms (78.26%), Space: 46.5 MB (100.00%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.ts
@@ -0,0 +1,14 @@
+function addToArrayForm(num: number[], k: number): number[] {
+     for(let i = num.length-1; i>=0 && k>0 ; i--){
+        num[i] += k;
+        k = Math.floor(num[i]/10);
+        num[i] = Math.floor(num[i]%10);
+        
+    }
+    
+    while(k>0){
+        num.unshift(Math.floor(k%10));
+        k = Math.floor(k/10);
+    }
+    return num;
+};
