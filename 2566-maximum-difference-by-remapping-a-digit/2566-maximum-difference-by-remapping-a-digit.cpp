ee5ea3c686e5b8ebb5f940cb9b3d15b3465ff592
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-18 22:22:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (25.00%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
@@ -3,13 +3,13 @@ public:
     int minMaxDifference(int num) {
         
         string ns = to_string(num);        
-        char first = ns[0];
+        char firstmax = ns[0];
         char firstmin = ns[0];
         //to find first digit which replace with 9 
         for(auto i : ns){
             if(i != '9')
             {
-                first = i;
+                firstmax = i;
                 break;
             }
         }
@@ -18,7 +18,7 @@ public:
         string mins = "";
         for(int i = 0; i<ns.size(); i++){
             
-            if(ns[i] == first)
+            if(ns[i] == firstmax)
                 maxs += '9';
             else
                 maxs += ns[i];
@@ -29,20 +29,6 @@ public:
                 mins += ns[i];
         }
         
-//          for(int i = 0; i<ns.size(); i++){
-            
-//             if(ns[i] == firstmin){
-//                 // maxs += '9';
-//                 mins += '0';
-//             }else{
-//                 // maxs += ns[i];
-//                 mins += ns[i];
-//             }
-//         }
-        
-        int ma = stoi(maxs);
-        int mi = stoi(mins);
-        cout << ma << " " << mi << endl;
         return stoi(maxs) - stoi(mins);
     }
 };
