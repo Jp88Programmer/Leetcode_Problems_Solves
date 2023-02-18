2d93e8803a79f3548f4cd93fcd584d7b589b82d5
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-18 22:08:42 +0530

    Time: 155 ms (100.00%), Space: 51.8 MB (100.00%) - LeetHub

---
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.ts
@@ -0,0 +1,5 @@
+function minimizeSum(nums: number[]): number {
+     let n = nums.length;
+    nums.sort((a,b) => a - b);
+    return n <= 3 ? 0 : Math.min(nums[n-2]-nums[1],nums[n-1]-nums[2],nums[n-3]-nums[0]);
+};
