8127e210c5827a2549eeb14d76ab8924cd5607f1
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-18 22:43:31 +0530

    Time: 135 ms (80.00%), Space: 47.5 MB (90.00%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minImpossibleOR(vector<int>& nums) {
         
-        /*
+        
         sort(nums.begin(),nums.end());
         
         int ans = 1;
@@ -13,11 +13,13 @@ public:
         }
         
         return ans;
-        */
+        
+        /*
          unordered_set<int> s(nums.begin(), nums.end());
         int a = 1;
         while (s.count(a))
             a <<= 1;
         return a;
+        */
     }
 };
