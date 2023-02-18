444626098492824edfbaf715289c37b8db047975
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-18 22:08:11 +0530

    Time: 163 ms (100.00%), Space: 51.9 MB (100.00%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.js
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var minimizeSum = function(nums) {
-     // int n = nums.size();
-     //    if(n<=3) return 0;
-     //    sort(nums.begin(),nums.end());
-     //    return min({nums[n-2]-nums[1],nums[n-1]-nums[2],nums[n-3]-nums[0]});
-
+  
     let n = nums.length;
     nums.sort((a,b) => a - b);
     return n <= 3 ? 0 : Math.min(nums[n-2]-nums[1],nums[n-1]-nums[2],nums[n-3]-nums[0]);
