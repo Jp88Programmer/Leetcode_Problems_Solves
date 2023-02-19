4122bfd162f3d96674f85a77724116b9be7f6cd4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-19 10:03:58 +0530

    Time: 75 ms (100.00%), Space: 41.8 MB (100.00%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
@@ -3,13 +3,10 @@
  * @return {number}
  */
 var minOperations = function(n) {
-    
             
-        if(n <= 0) return 0 ;
-        if(n == 1) return 1 ;
-        
+    if(n <= 0) return 0 ;
+    if(n == 1) return 1 ; 
     let p = 1;
     while(p*2 < n) p *= 2;
-    if(p == n) return 1;
     return Math.min(minOperations(n-p),minOperations(p*2-n)) + 1;
 };
