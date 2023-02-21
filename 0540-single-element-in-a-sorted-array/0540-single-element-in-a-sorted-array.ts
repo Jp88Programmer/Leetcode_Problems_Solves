77c15c55f3d312b3f00a7d980f8aa11150d3af39
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-21 22:53:35 +0530

    Time: 95 ms (7.41%), Space: 45.3 MB (100.00%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.ts
@@ -0,0 +1,19 @@
+function singleNonDuplicate(arr: number[]): number {
+    let n = arr.length;
+    
+    if(n == 1) return arr[n-1];
+        
+        let s = 0;
+        let e = n - 2;
+        
+        while(s <= e){
+            let m = (s + e) >> 1;
+            if(arr[m] == arr[m^1])
+                s = m + 1;
+            else 
+                e = m - 1;
+            
+        }
+        
+        return arr[s];
+};
