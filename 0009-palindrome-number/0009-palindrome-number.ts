9fd3418edd1e3b54fedce395d03a1e5d571b5511
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-24 23:09:21 +0530

    Time: 188 ms (53.40%), Space: 52.3 MB (12.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -0,0 +1,9 @@
+function isPalindrome(x: number): boolean {
+    let str = x.toString();
+    let s = 0,e = str.length-1;
+    while(s < e){
+        if(str[s++] !== str[e--])
+            return false;
+    }
+    return true;
+};
