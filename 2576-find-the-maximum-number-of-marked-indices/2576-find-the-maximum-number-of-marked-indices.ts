870f1c0d529c36ae529917ad32bd9405384e5929
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-02-26 10:20:46 +0530

    Time: 216 ms (100.00%), Space: 55.4 MB (100.00%) - LeetHub

---
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.ts
@@ -0,0 +1,14 @@
+function maxNumOfMarkedIndices(nums: number[]): number {
+    
+    nums.sort((a,b)=> a - b);
+    
+    let j = nums.length-1;
+    let count = 0;
+    for(let i = Math.floor(nums.length/2) - 1; i>=0; i--){
+        if(nums[i]*2 <= nums[j]){
+            j--;
+            count+=2;
+        }
+    }
+    return count;
+};
