2cbea1b4859f55659b58749b5d51ddf9774d591d
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-03-13 07:47:12 +0530

    Time: 70 ms (86.56%), Space: 44 MB (96.92%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    let count = 1;
+    
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]!=nums[i-1])
+            nums[count++] = nums[i];
+    }
+    return count;
+    
+};
