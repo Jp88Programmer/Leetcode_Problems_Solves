70179458406d74cefde1637cace87a18c6dcf0e5
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-03-22 07:27:03 +0530

    Time: 67 ms (69.09%), Space: 43 MB (43.64%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -4,13 +4,8 @@
  */
 var maxPower = function(s) {
     let count=1,ans=0;
-    
     for(let i = 0; i<s.length; i++){
-        if(s.charAt(i) != s.charAt(i+1)){
-            count = 1;
-        }else{
-            count++;
-        }
+        count = s.charAt(i) === s.charAt(i+1) ? ++count : 1;
         ans = Math.max(count,ans);
     }
     return ans;
