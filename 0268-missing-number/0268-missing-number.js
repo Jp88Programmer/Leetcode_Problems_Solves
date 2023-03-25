ed1408bf8e9fc18b5a29728295dfa1436ff173fb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-03-25 23:22:41 +0530

    Time: 72 ms (63.05%), Space: 44.1 MB (78.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,14 +3,8 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    
     let n = nums.length;
-    
     let sum = Math.floor((n * (n+1)) / 2);
-    
     let sum1 = nums.reduce((acc,e)=> acc+e);
-    
     return sum - sum1;
-    
-    
 };
