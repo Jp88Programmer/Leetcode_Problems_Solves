b7ec7543c0a64ae42c577fcc38aa60bfae0eeb9d
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-03 07:57:16 +0530

    Time: 163 ms (53.43%), Space: 50.1 MB (37.79%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.js
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -5,13 +5,19 @@
  */
 var numRescueBoats = function(people, limit) {
     
-    people.sort((a,b)=> b - a)
+    people.sort((a,b)=> a - b)
     let s = 0,e = people.length-1;
+    let count  = 0;
     while(s <= e){
         if(people[s] + people[e] <= limit){
+            s++;
             e--;
+            count++;
         }
-        s++;
+        else{
+            e--;
+            count++;
+        } 
     }
-    return s ;
+    return count ;
 };
