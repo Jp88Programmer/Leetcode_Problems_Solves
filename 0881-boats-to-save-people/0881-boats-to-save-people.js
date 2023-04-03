97aece34525ded2b5cd3e4c2f95b6e70e0695d46
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-03 07:55:00 +0530

    Time: 152 ms (89.69%), Space: 49.3 MB (86.26%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.js
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -5,61 +5,13 @@
  */
 var numRescueBoats = function(people, limit) {
     
-    people.sort((a,b)=> a - b)
-    let sum = 0,count = 0;
-    let v = new Array(people.length).fill(false);
+    people.sort((a,b)=> b - a)
     let s = 0,e = people.length-1;
-    let flag = 0;
-    while(s < e){
-        
+    while(s <= e){
         if(people[s] + people[e] <= limit){
-            count++;
-            v[s] = true;
-            v[e] = true;
-            s++;
             e--;
         }
-        else if(people[e] <= limit){
-            v[e] = true;
-            e--;
-            count++;
-        }else if(people[s] <= limit){
-            v[s] = true;
-            s++;
-            count++;
-        }
+        s++;
     }
-    
-    if(s == e && people[s] <= limit){
-        count++;
-        v[s] = true;
-    }
-    
-    if(!v[s] && people[s] <= limit){
-        count++;
-        v[s] = true;
-    }
-    
-    if(!v[e] && people[e] <= limit){
-        count++;
-        v[e] = true;
-    }
-    
-    // count++;
-//     for(let i = 0; i<people.length; i++){
-        
-//         if(sum + people[i] <= limit){
-//             sum += people[i];
-//         }else{
-//             sum = 0;
-//             count++;
-//             i--;
-//         }
-//     }
-    
-//     if(sum > 0){
-//         count++;
-//     }
-    
-    return count;
+    return s ;
 };
