77f7619cc19a05ba6df402406306fbe94ed19a2e
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-17 22:53:56 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.js
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    for(let i=0;i<t;i++)
+    {
+        let input_line = readLine().split(' ');
+        let N = parseInt(input_line[0]);
+        input_line = readLine().split(' ').map(x=>parseInt(x));
+        let arr = new Array(N);
+        for(let i=0;i<N;i++){
+            arr[i]=input_line[i];
+        }
+        let obj = new Solution();
+        let ans = obj.maxSubarraySum(arr, N);
+        if(ans==-0)
+            ans=0;
+        console.log(ans);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number} 
+*/
+class Solution {
+    //Function to find the sum of contiguous subarray with maximum sum.
+    maxSubarraySum(nums, N){
+        // code here
+         
+    let maxSum = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    
+    for(let i = 0; i<N; i++){
+        sum += nums[i];
+        maxSum = Math.max(maxSum,sum);
+        if(sum < 0) sum = 0;
+    }
+    return maxSum ;
+    } 
+}
