ae8860b4f8f76acadd6c29414460e738ad260d4b
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-18 07:18:53 +0530

    Time: 434 ms (33.33%), Space: 89.8 MB (60.61%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.ts
@@ -0,0 +1,16 @@
+function rearrangeArray(nums: number[]): number[] {
+    let ans = new Array(nums.length).fill(0);
+    let evenIndex = 0;
+    let oddIndex = 1;
+    
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i]>0){
+            ans[evenIndex] = nums[i];
+            evenIndex += 2;
+        }else{
+            ans[oddIndex] = nums[i];
+            oddIndex += 2;
+        }  
+    }
+    return ans;
+};
