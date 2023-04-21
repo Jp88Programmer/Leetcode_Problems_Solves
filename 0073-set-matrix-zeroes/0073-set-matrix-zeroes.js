f8b0deca5cb879d0eb678a2b95a71a3167d58f37
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-21 07:55:01 +0530

    Time: 73 ms (89.48%), Space: 45.1 MB (46.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -6,24 +6,16 @@
 const modRowCols = function(matrix,row,col) {
     
     
-    for(let i = 0; i<matrix.length; i++){
-            if(row == i){
-                // matrix[i].fill(0);
-                for(let j = 0; j<matrix[0].length; j++){
-                    matrix[i][j] = 0;
-                }
-                
-            }
+        for(let i = 0; i<matrix.length; i++){
+
             for(let j = 0; j<matrix[i].length; j++){
-                if(col == j){
-                    for(let k = 0; k<matrix.length; k++){
-                        matrix[k][j] = 0
-                    }
-                }
+
+                if(row == i || col == j)
+                    matrix[i][j] = 0;
             }
         }
-    
-}
+
+    }
 var setZeroes = function(matrix) {
     
     let calrc = [];
