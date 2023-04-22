268d9c6aed1b1ed8fe54bd27bcf82bda2ede113a
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-22 11:55:09 +0530

    Time: 68 ms (34.86%), Space: 44.3 MB (69.04%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -2,34 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var binarySearchForNeg = function(arr){
+var binarySearch = function(arr,isPos = false){
     let s = 0, e = arr.length - 1;
     while(s<=e){
         let m = s + Math.floor((e - s)/2);
         if(arr[m] < 0){
             s = m + 1;
-        }else{
+        }else if(arr[m] > 0){
             e = m - 1;
+        }else{
+            if(isPos){
+                s = m + 1;
+            }else
+                e = m - 1;
         }
     }
+    
+    if(isPos)
+        return e;
     return s;
-}
-var binarySearchForPos = function(arr){
-    let s = 0, e = arr.length - 1;
-    while(s<=e){
-        let m = s + Math.floor((e - s)/2);
-        if(arr[m] > 0){
-            e = m - 1;
-        }else{
-            s = m + 1;
-        }
-    }
-    return e;
 }
 var maximumCount = function(nums) {
     let s = 0,e = nums.length - 1;
     let neg = 0,pos = 0;
-    neg = binarySearchForNeg(nums) ;
-    pos = nums.length - binarySearchForPos(nums) - 1;
+    neg = binarySearch(nums) ;
+    pos = nums.length - binarySearch(nums,true) - 1;
     return Math.max(neg,pos);
 };
