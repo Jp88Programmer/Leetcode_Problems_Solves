371ac3dd392138e029822b8e1b96e41c1ab7f7fa
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-04-24 08:19:58 +0530

    Time: 66 ms (26.13%), Space: 41.9 MB (67.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,8 +6,6 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
     nums1.splice(m,n,...nums2)
-    // console.log(nums2)
     nums1.sort((a,b)=> a - b);
 };
