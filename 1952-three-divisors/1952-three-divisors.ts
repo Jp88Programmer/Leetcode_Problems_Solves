795c7b47c9f39b16836e04d811503e8585710883
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-02 21:42:10 +0530

    Time: 57 ms (90.91%), Space: 43.7 MB (22.73%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.ts
@@ -0,0 +1,12 @@
+function isThree(n: number): boolean {
+    if(n == 1) return false;
+    let count = 1,i = 2;
+    while(i<=n){
+        if(n % i == 0)
+            count++;
+        if(count > 3)
+            return false;
+        i++;
+    }
+    return count === 3;
+};
