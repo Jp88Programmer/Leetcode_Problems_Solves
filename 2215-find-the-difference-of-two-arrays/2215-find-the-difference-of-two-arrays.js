b10b0a76dd419072910008140fdffa18584a0703
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-03 23:36:54 +0530

    Time: 133 ms (40.00%), Space: 48.2 MB (79.08%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -5,33 +5,8 @@
  */
 var findDifference = function(nums1, nums2) {
     
-    let map = new Map();
-    let ans = []
-    let res = []
-    nums2.forEach((e)=>{
-        map.set(e,map.get(e) ? map.get(e)+1 : 1);
-    })
-    
-    nums1.forEach((e)=>{
-        if(!map.has(e)){
-            ans.push(e);
-            map.set(e,map.get(e) ? map.get(e)+1 : 1);
-        }
-    })
-    res.push(ans);
-    
-    map = new Map();
-    ans = []
-     nums1.forEach((e)=>{
-        map.set(e,map.get(e) ? map.get(e)+1 : 1);
-    })
-    
-    nums2.forEach((e)=>{
-        if(!map.has(e)){
-            ans.push(e);
-            map.set(e,map.get(e) ? map.get(e)+1 : 1);
-        }
-    })
-    res.push(ans);
-    return res;
+    return [
+        [...new Set(nums1)].filter((n)=> !nums2.includes(n)),
+        [...new Set(nums2)].filter((n)=> !nums1.includes(n))
+    ]
 };
