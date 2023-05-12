d8e530c690cecead213efc43ce89edbd647b8ddf
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-12 22:30:05 +0530

    Time: 63 ms (54.55%), Space: 42.9 MB (62.03%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,19 @@
+function once<T extends (...args: any[]) => any>(fn: T): 
+ ((...args: Parameters<T>) => ReturnType<T> | undefined) {
+ let flag = 0;
+    return function(...args){
+        // console.log(args)
+        if(flag == 0){
+            flag = 1;
+            return fn(...args);
+        }
+    }
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
