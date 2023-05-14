16ddea837029262f94e7f669d912481da04d99f5
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-14 12:52:26 +0530

    Time: 258 ms (53.85%), Space: 69 MB (23.08%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    // int maxcount = 0;
+    
+    int dfs(int row,int col,int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        int count = 0;
+        if(col > n || col < 0 || row > m || row < 0)
+            return 0;
+        // count++;
+        // maxcount = max(maxcount,count);
+        if(row - 1 >= 0 && col + 1 < n && (grid[row][col] < grid[row-1][col+1]) ){
+            if(dp[row-1][col+1] != 0)
+                count = max(count,dp[row-1][col+1]);
+            else
+                count = max(count, dfs(row-1,col+1,m,n,grid,dp) + 1);
+        }
+           
+        
+         if(col + 1 < n && (grid[row][col] < grid[row][col+1]) ){
+              if(dp[row][col+1] != 0)
+                count = max(count,dp[row][col+1]);
+            else
+                count = max(count, dfs(row,col+1,m,n,grid,dp) + 1);
+         }
+            
+        
+        if(row + 1 < m && col + 1 < n && (grid[row][col] < grid[row+1][col+1]) ){
+             if(dp[row+1][col+1] != 0)
+                count = max(count,dp[row+1][col+1]);
+            else
+                count = max(count, dfs(row+1,col+1,m,n,grid,dp) + 1);
+        }
+        
+        return dp[row][col] = count;
+    }
+    int maxMoves(vector<vector<int>>& grid) {
+        
+        int minnum = INT_MAX;
+        int minIndex = 0;
+        int m = grid.size(); 
+        int n = grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        int count = 0;
+        int res = 0;
+        for(int  i = 0; i<grid.size(); i++){
+            // if(minnum > grid[i][0]){
+            //     minnum = grid[i][0];
+            //     minIndex = i;
+            // }
+           count =  dfs(i,0,m,n,grid,dp);
+            res = max(res,count);
+        }
+        
+        // cout << minnum << endl;
+        return res;
+        
+        
+    }
+};
