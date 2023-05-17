cbb970c9a68c8b404474192d914dbbac38d71a7d
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-17 23:09:15 +0530

    Time: 61 ms (71.57%), Space: 43.8 MB (85.83%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -0,0 +1,7 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    nums1.splice(m,n,...nums2)
+    nums1.sort((a,b)=> a - b);
+};
