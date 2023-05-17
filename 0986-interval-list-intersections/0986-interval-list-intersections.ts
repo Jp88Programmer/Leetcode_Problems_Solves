5f3a8c7614026f9b85498ec1665036375c5ace36
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-17 23:42:30 +0530

    Time: 104 ms (55.42%), Space: 50.3 MB (53.01%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.ts
@@ -0,0 +1,11 @@
+function intervalIntersection(fl: number[][], sl: number[][]): number[][] {
+     const ans = [];
+    let m = fl.length,n = sl.length;
+    for(let i = 0, j = 0; i<m && j < n;){
+        if(fl[i][0] <= sl[j][1] && fl[i][1] >= sl[j][0]){
+            ans.push([Math.max(fl[i][0],sl[j][0]),Math.min(fl[i][1],sl[j][1])]);
+        }    
+        fl[i][1] < sl[j][1]  ? i++ : j++;
+    }
+    return ans;
+};
