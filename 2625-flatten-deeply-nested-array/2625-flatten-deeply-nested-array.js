d536c513b4465c519129acfb67cfd9fb7a23e846
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-26 22:55:27 +0530

    Time: 184 ms (59.67%), Space: 97.4 MB (31.82%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat = function (arr, n) {
+    
+    if(n == 0) return arr;
+    let ans = []
+    for(let i = 0; i<arr.length; i++){
+        if(Array.isArray(arr[i])){
+            ans.push(...flat(arr[i],n-1));
+        }else{
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
