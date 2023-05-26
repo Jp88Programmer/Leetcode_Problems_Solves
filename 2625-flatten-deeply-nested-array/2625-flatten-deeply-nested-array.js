1551618129407b415a80b84a388d8c1129045bba
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-05-26 22:57:43 +0530

    Time: 231 ms (17.47%), Space: 103.1 MB (10.27%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -5,24 +5,21 @@
  */
 var flat = function (arr, n) {
     
-    let i = 0,j = 0;
-    function flatternarr(arr,i,j,n){
+    let i = 0;
+    function flatternarr(arr,i,n){
         if(i === n){
             return arr;
         }
-       
         let ans = [];
         i++;
         arr.map(e => {
             if(Array.isArray(e)){
-                ans.push(...flatternarr(e,i,j,n));
+                ans.push(...flatternarr(e,i,n));
             }else{
                 ans.push(e);
-                j++;
             }
         })
-        
         return ans;
     }
-    return flatternarr(arr,i,j,n);
+    return flatternarr(arr,i,n);
 };
