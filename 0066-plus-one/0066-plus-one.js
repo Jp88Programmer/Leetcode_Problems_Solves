3bbb132ee2438c31176802b67fd944b4621e1c04
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-04 16:50:06 +0530

    Time: 53 ms (84.57%), Space: 42.2 MB (36.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -7,17 +7,13 @@ var plusOne = function(digits) {
     let n = digits.length;
     let carry = Math.floor((digits[n-1]+1) / 10);
     digits[n-1] = (digits[n-1]+1) % 10;
-    console.log(digits[n-1]);
-    
     for(let i = digits.length-2; i>=0 && carry > 0; i--){
         let val = digits[i] + carry;
         carry = Math.floor(val / 10);
         digits[i] = val % 10;
     }
-    
     if(carry > 0){
         digits.splice(0,0,carry);
     }
-    
     return digits;
 };
