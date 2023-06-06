7e09151b7e41577b30faab66dd835f3686b44077
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-06 22:17:00 +0530

    Added solution - LeetHub

---
+++ Sort in specific order - GFG/sort-in-specific-order.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        obj.sortIt(arr, n);
+        printList(arr, n);
+        
+    }
+}// } Driver Code Ends
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {void}
+*/
+
+class Solution{
+    sortIt(arr,n){
+        //code here
+        let odd = [],even = [];
+        arr.map(e=>{
+            if(e % 2)
+                odd.push(e);
+            else
+                even.push(e);
+        })
+        odd.sort((a,b)=>b-a);
+        even.sort((a,b)=>a-b);
+        // console.log(even)
+        const ans = [...odd,...even];
+        ans.map((e,i)=>arr[i]=e);
+        
+    }
+}
+
+
