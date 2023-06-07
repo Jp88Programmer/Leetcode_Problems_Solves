86791f8c48a6f94da09de189a4e1cd1baa43dec9
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-07 23:03:58 +0530

    Time: 59 ms (84.51%), Space: 45.1 MB (21.13%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.ts
@@ -0,0 +1,12 @@
+function canMakeArithmeticProgression(arr: number[]): boolean {
+    let maxnum = Math.max(...arr);
+    let minnum = Math.min(...arr);
+    if((maxnum - minnum) % (arr.length - 1) != 0) return false;
+    let step = Math.floor((maxnum - minnum) / (arr.length - 1));
+    if(step == 0) return true;
+    const set = new Set(arr);
+    for(let i =1; i<arr.length; i++){
+        if(!set.has(minnum + i * step)) return false;
+     }
+    return true;
+};
