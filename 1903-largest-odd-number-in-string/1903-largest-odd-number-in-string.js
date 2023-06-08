d952a0d88460d78e6bddf28e0c9cb414a6d0e952
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-08 22:09:39 +0530

    Time: 68 ms (78.52%), Space: 45.5 MB (77.18%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    
+    let i = 0;
+    for(i = num.length - 1; i>=0; i--)
+        if(parseInt(num[i]) % 2 != 0) break;
+    return num.substr(0,i+1);
+};
