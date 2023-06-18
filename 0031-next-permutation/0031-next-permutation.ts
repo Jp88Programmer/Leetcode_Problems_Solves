eff7be6fec0f76431b00a8050510c9e79ce38b16
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-18 22:46:23 +0530

    Time: 74 ms (61.42%), Space: 44.3 MB (100.00%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.ts
@@ -0,0 +1,30 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+     let n = nums.length ;
+    let index = -1;
+    for(let i = n - 2; i >= 0; i--){
+        if(nums[i] < nums[i+1]){
+            index = i;
+            break;
+        }
+    }
+    if(index == -1)
+        nums.reverse();
+    else{
+        let id = index + 1;
+        for(let i = n - 1; i>=id; i--){
+            if(nums[i]>nums[index]){
+                [nums[i],nums[index]] = [nums[index],nums[i]];
+                break;
+            }
+        }
+        let arr = nums.slice(index+1,n);
+        arr.reverse();
+        // console.log(arr)
+        for(let i = id,k = 0; i<n && k < arr.length; i++){
+            nums[i] = arr[k++];
+        }
+    }
+};
