73a03375ed706d9eb6db539774c1e72bca5d0c72
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-19 10:35:20 +0530

    Time: 59 ms (59.94%), Space: 42.2 MB (45.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,7 +3,6 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var trans = function(matrix)  {
-    
     for(let i = 0; i<matrix.length; i++){
         for(let j = i+1; j < matrix[i].length; j++){
             [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]];
