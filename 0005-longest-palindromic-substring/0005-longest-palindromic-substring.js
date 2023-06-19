2fd663fcc50081d974cae98315a5cc939231fc8e
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-06-19 14:40:03 +0530

    Time: 82 ms (83.56%), Space: 42.4 MB (97.56%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+    let mLen = 1;
+    let start = 0;
+    let n = s.length;
+    for(let i = 1; i<s.length; i++){
+        
+        let low = i - 1;
+        let high = i + 1;
+        
+        while(high < n && s[high] === s[i])
+            high++;
+        
+        while(low>=0 && s[low] === s[i])
+            low--;
+        
+        while(low >= 0 && high < n && s[low] === s[high]){
+            low--;
+            high++;
+        }
+        
+        let len = high - low - 1;
+        if(mLen < len){
+            mLen = len;
+            start = low + 1;
+        }
+    }
+    
+    return s.substr(start,mLen);
+};
