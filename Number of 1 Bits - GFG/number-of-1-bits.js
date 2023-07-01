8a76484b137715fb79e7f3351b9f5c2927ac3433
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-01 23:02:25 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.js
+++ Number of 1 Bits - GFG/number-of-1-bits.js
@@ -1,61 +1,34 @@
 //{ Driver Code Starts
-//Initial Template for javascript
+#include <bits/stdc++.h>
+using namespace std;
 
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-function main() {
-    let t = parseInt(readLine());
-    let i = 0;
-    for(;i<t;i++)
-    {
-        let n = parseInt(readLine());
-        let obj = new Solution();
-        console.log(obj.setBits(n));
-        
-    }
-}
 // } Driver Code Ends
-
-
-//User function Template for javascript
-
-/**
- * @param {number} n
- * @returns {number}
-*/
-
 class Solution {
-    
-    setBits(n)
-    {
-        //your code here
-        let count = 0;
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int count = 0;
         while(n){
             count += n & 1;
             n >>= 1;
         }
         return count;
     }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
 }
+
+// } Driver Code Ends
