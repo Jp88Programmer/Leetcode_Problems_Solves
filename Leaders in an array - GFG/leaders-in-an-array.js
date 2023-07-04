c3f3857357ab838a7aaf339e1446835f5b3ac4ef
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-04 22:58:37 +0530

    Added solution - LeetHub

--- Leaders in an array - GFG/leaders-in-an-array.js
+++ Leaders in an array - GFG/leaders-in-an-array.js
@@ -1,74 +1,60 @@
 //{ Driver Code Starts
-//Initial Template for javascript
-'use strict';
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-function main() {
-    let t = parseInt(readLine());
-    let i = 0;
-    for(;i<t;i++)
-    {
-        let n = parseInt(readLine());
-        let a = new Array(n);
-        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
-        for(let i=0;i<n;i++)
-            a[i] = input_ar1[i];
-        let obj = new Solution();
-        let ans = obj.leaders(a, n);
-        let S = '';
-        for(let i=0;i<ans.length;i++)
-        {
-            S+=ans[i];
-            S+=' ';
-        }
-        console.log(S);
-    }
-}
 // } Driver Code Ends
+    
 
-
-//User function Template for javascript
-
-/**
- * @param {number[]} a
- * @param {number} n
- * @returns {number[]}
- */
- 
-class Solution {
+class Solution{
     //Function to find the leaders in the array.
-    leaders(a, n){
-        // code here
-        let m = a[n-1];
-        let ans = [m];
-        for(let i = n -2 ; i>=0; i--){
-            if(a[i]>=m){
-                ans.push(a[i]);
-                m = a[i];
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        int maxVal = a[n-1];
+        vector<int>ans(1,maxVal);
+        for(int i = n - 2; i>=0; i--){
+            if(a[i]>=maxVal){
+                ans.push_back(a[i]);
+                maxVal = a[i];
             }
         }
-        ans.reverse();
+        reverse(ans.begin(),ans.end());
         return ans;
     }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
 }
+
+// } Driver Code Ends
