a1f2b28826d6589214965631146d8309c4ddf299
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-19 21:34:03 +0530

    Time: 512 ms (41.38%), Space: 89.6 MB (94.24%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,58 +3,16 @@ public:
     static bool cmp(vector<int>&a,vector<int>&b){
         return a[1]<b[1];
     }
+    
     int eraseOverlapIntervals(vector<vector<int>>& inter) {
-        
-        /*
-         sort(inter.begin(),inter.end());
-         vector<vector<int>> ans;
-         int s = inter[0][0];
-         int e = inter[0][1];
-         for(int i=1; i<inter.size(); i++){
-             
-             if(inter[i][0] < e && inter[i][1] > s){
-                 s = min(inter[i][0],s);
-                 e = max(inter[i][1],e);
-             }else{
-                //  ans.push_back({s,e});
-                ans.push_back(inter[i]);
-                //  s = inter[i][0];
-                //  e = inter[i][1];
-             }
-         }
-         
-         ans.push_back({s,e});
-          
-        //  cout << s << " " << e << endl;
-         
-         int count = 0;
-         for(int i = 0; i<inter.size(); i++){
-             
-             if(inter[i][0] == s && inter[i][1] == e)
-                count++;
-         }
-         
-         return count ;
-         */
-        
         sort(inter.begin(),inter.end(),cmp);
-        
-        for(auto i : inter){
-            cout << i[0] << " "<< i[1] << endl;
-        }
-        
-        int pre = 0;
-        int count = 0;
+        int count=0,overlapEndpoint=0;
         for(int i = 1; i<inter.size(); i++){
-            
-            if(inter[i][0] < inter[pre][1]){
+            if(inter[i][0] < inter[overlapEndpoint][1])
                 count++;
-            }else{
-                pre = i;
-            }
+            else
+                overlapEndpoint = i;
         }
-        
-        return count;
-        
+    return count;
     }
 };
