9dcef19eb38817246c42f025e11031444b6f987f
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-23 10:55:34 +0530

    Time: 72 ms (68.94%), Space: 47.6 MB (14.56%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,15 +4,23 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    let s = -1,ans = 0,count = 0;
-    for(let i  =0; i < nums.length ; i++){
-        if(nums[i] == 0) count++;
-        while(count > k){
-            s++;
-            if(nums[s] == 0) count--;
+    
+    let j = 0,ans = 0,one = 0,zero = 0;
+    for(let i = 0; i<nums.length ; i++){
+        
+        if(nums[i] == 1) one++;
+        if(nums[i] == 0) zero++;
+        if(zero <= k) ans = Math.max(ans,one+zero);
+        while(zero > k){
+            if(nums[j] == 1){
+                j++;
+                one--;
+            }else if(nums[j] == 0){
+                j++;
+                zero--;
+            }
+            if(zero <= k) ans = Math.max(ans,one+zero);
         }
-        let len = i - s;
-        ans = Math.max(ans, len);
     }
     return ans;
 };
