38cab85cdce290a96001f605e58a4f262cd37d2d
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-24 22:31:48 +0530

    Time: 19 ms (35.09%), Space: 7 MB (72.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    char maxfr(map<char,int>&m){
-        // char ans = a; 
-        int fr = 1;
-        for(auto x : m){
-            if(fr <= x.second){
-                fr = x.second;
-            }
-        }
-        return fr;
-    }
     int characterReplacement(string s, int k) {
         map<char,int>m;
         int j = 0;
@@ -23,12 +13,11 @@ public:
                 res = max(res,len);
             
             while(len - maxfr > k){
-                m[s[j]] = m[s[j]] == 0 ? 0 : m[s[j]] - 1;
+                m[s[j]]--;
                 maxfr = max(maxfr,m[s[j]]);
                 j++;
                 len = i - j + 1;
             }
-            // res = max(res,len);
         }
         return res;
     }
