97e337c69a7334d04028dcf0aeab4b2d54ef4a59
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-07-30 23:23:01 +0530

    Time: 15 ms (99.45%), Space: 11.1 MB (41.96%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -4,16 +4,11 @@ public:
         return ch == 'a' || ch == 'e' || ch == 'i' ||ch == 'o' ||ch == 'u';
     }
     long long countVowels(string w) {
-        long long res = 0,n=w.size();
-        
-        for(int i = 0; i < w.size(); i++){
-            if(isVowel(w[i])){
-               long long sum = i + 1;
-               long long sum1 = n - 1 - i;
-                long long sumr = (sum-1)*sum1;
-                res += sum + sum1 + sumr;
-            }
-            
+        long long res = 0,n=w.size(),sum = 0;
+        for(int i = n-1;i>=0; i--){
+            if(isVowel(w[i]))
+                sum += n - i;
+            res += sum;
         }
         return res;
     }
