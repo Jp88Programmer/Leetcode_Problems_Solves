237f0a24842b5b6f3be1f3feec68d6f3470a9887
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-08-08 22:27:20 +0530

    Time: 40 ms (99.23%), Space: 41.4 MB (97.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,34 +3,18 @@
  * @param {number} target
  * @return {boolean}
  */
-const bin = (arr,l,h,t)=>{
-    
-    while(l<=h){
-        let m = (l + h) >> 1;
-        if(arr[m] == t) return true;
-        else if(arr[m] < t) l = m + 1;
-        else 
-            h = m - 1;
-    }
-    
-    return false;
-}
+
 var searchMatrix = function(matrix, target) {
     
-    let m = matrix.length;
-    let n = matrix[0].length;
-    let s = 0, e = m * n -1;
-    
-    while(s <= e){
-        let midIndex = Math.floor((s + e) / 2);
-        let midVal = matrix[Math.floor(midIndex / n)][midIndex % n];
-        if(midVal == target)
+    let m = matrix.length,n = matrix[0].length;
+    let row = 0,col = n - 1;
+    while(row<m && col >= 0){
+        if(matrix[row][col] === target)
             return true;
-        else if (midVal < target)
-            s = midIndex + 1;
+        else if(matrix[row][col] < target)
+            row++;
         else
-            e = midIndex - 1;
+            col--;
     }
-    
     return false;
 };
