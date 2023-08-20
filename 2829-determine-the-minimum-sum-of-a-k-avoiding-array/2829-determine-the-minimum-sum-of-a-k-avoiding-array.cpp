5b5f1f1d31e4ba432ac9224f7437882e644c61a1
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-08-20 11:38:37 +0530

    Time: 25 ms (70.00%), Space: 10.8 MB (100.00%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -1,30 +1,5 @@
 class Solution {
 public:
-    bool sumcom(vector<int>&v,int sum,int k){
-        for(int i = 0; i<v.size(); i++){
-            if(sum + v[i] == k) return false;
-        }
-        return true;
-    }
-    int fun(vector<int>&v,int n,int k){
-        
-        v.push_back(1);
-        for(int i = 2; i<=50; i++){
-            if(v.size() == n)
-                break;
-            if(sumcom(v,i,k)){
-                v.push_back(i);
-            }
-            
-        }
-        
-        int sum = 0;
-        for(int i = 0; i<v.size(); i++){
-            cout << v[i] << " ";
-            sum += v[i];
-        }
-        return sum;
-    }
     int minimumSum(int n, int k) {
       
         unordered_set<int>s;
