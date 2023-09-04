3da9b93b90cf9d03a62a1ec11258c24b047fbbf4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-09-04 23:31:33 +0530

    Time: 19 ms (91.21%), Space: 17.4 MB (56.94%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,8 +3,6 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        
-        vector<int>div;
         int i = 0,j = 0;
         int count =0;
         for(; i < g.size() && j < s.size();j++){
