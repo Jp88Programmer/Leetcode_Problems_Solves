9c7e078df8a5b85b3adf653f81a5314ebb0e5b71
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-09-29 22:52:08 +0530

    Time: 128 ms (39.09%), Space: 96.9 MB (9.17%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -6,11 +6,6 @@ public:
             ans1 &= nums[i] <= nums[i-1];
             ans2 &= nums[i] >= nums[i-1];
         }
-        //  for(int i = 1; i<nums.size(); i++){
-        //     if(nums[i] > nums[i-1]){
-        //         ans2 = false;
-        //     }
-        // }
         return ans1 || ans2;
     }
 };
