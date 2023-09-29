c9056708c83e389a0f090b2e2124bf8cc62aebfc
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-09-29 22:51:53 +0530

    Time: 129 ms (35.19%), Space: 96.8 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -3,15 +3,14 @@ public:
     bool isMonotonic(vector<int>& nums) {
         bool ans1 = true,ans2 = true;
         for(int i = 1; i<nums.size(); i++){
-            if(nums[i] < nums[i-1]){
-                ans1 = false;
-            }
+            ans1 &= nums[i] <= nums[i-1];
+            ans2 &= nums[i] >= nums[i-1];
         }
-         for(int i = 1; i<nums.size(); i++){
-            if(nums[i] > nums[i-1]){
-                ans2 = false;
-            }
-        }
+        //  for(int i = 1; i<nums.size(); i++){
+        //     if(nums[i] > nums[i-1]){
+        //         ans2 = false;
+        //     }
+        // }
         return ans1 || ans2;
     }
 };
