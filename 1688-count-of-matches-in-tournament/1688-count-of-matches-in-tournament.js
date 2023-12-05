9deb4e20bfbabb91ef5a722e7112e4eaf11dc2ae
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-05 22:16:08 +0530

    Time: 49 ms (71.30%), Space: 42 MB (22.66%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -6,13 +6,9 @@ var numberOfMatches = function(n) {
     
     let ans = 0;
     while(n != 1){
-        if(n % 2 === 0){
-            ans += Math.floor(n/2);
-            n = Math.floor(n/2);
-        }else{
-            ans += Math.floor((n-1)/2);
-            n = Math.floor((n-1)/2)+1;
-        }
+        const matches = Math.floor(n/2);
+        ans += matches;
+        n = n - matches;
     }
     return ans;
 };
