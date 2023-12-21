fc713e18063f75ceeadf39ee678d11bcec3919eb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-21 23:48:29 +0530

    Time: 19 ms (7.34%), Space: 18.9 MB (52.16%) - LeetHub

--- 2961-double-modular-exponentiation/2961-double-modular-exponentiation.cpp
+++ 2961-double-modular-exponentiation/2961-double-modular-exponentiation.cpp
@@ -3,7 +3,6 @@ class Solution {
 public:
     vector<int> getGoodIndices(vector<vector<int>>& v, int target) {
         vector<int>ans;
-        
         for(int i = 0; i<v.size(); i++){
             int a = v[i][0],b = v[i][1],c = v[i][2],m = v[i][3];
             a = a % 10;
@@ -14,7 +13,6 @@ public:
             for(int p = 1; p<=c; p++){
                 x = (x * t) % m;
             }
-            cout << x << endl;
             if(x == target)
                 ans.push_back(i);
         }
