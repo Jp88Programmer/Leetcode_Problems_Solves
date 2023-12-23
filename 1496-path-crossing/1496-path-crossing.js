004293e37d5c6061cb0eda0e18b34d86f2edba50
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-23 21:48:58 +0530

    Time: 33 ms (100.00%), Space: 42 MB (74.10%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -15,35 +15,17 @@ var direction = function(dir){
     }
 }
 var isPathCrossing = function(path) {
-   
-    // N = x = 0 y = 1
-    // S = x = 0 y = -1
-    // E = x = 1 y = 0
-    // W = x = -1 y = 0
-    
     let m = new Map();
     let x = 0,y = 0;
-    m.set('0+0',1);
+    m.set('0,0',1);
     for(let p of path){
         let corr = direction(p);
         x += corr[0],y += corr[1];
-        console.log(x,y);
-        let str = [x,y].join("+");
-        if(m.has(str)){
+        let str = [x,y].join();
+        if(m.has(str))
             return true;
-        }else{
+        else
             m.set(str,1);
-        }
     }
-    // path.split("").map(p => {
-    //     let corr = direction(p);
-    //     x += corr[0],y += corr[1];
-    //     let str = [x,y].join("");
-    //     if(m.has(str)){
-    //         return true;
-    //     }else{
-    //         m.set(str,1);
-    //     }
-    // })
     return false;
 };
