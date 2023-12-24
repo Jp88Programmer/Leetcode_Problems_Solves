8b5e6184f42bc9fe85ed61c50469ab1de5109d39
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-24 12:43:00 +0530

    Time: 58 ms (23.75%), Space: 41.9 MB (58.82%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -4,11 +4,13 @@
  */
 var sortColors = function(nums) {
     let n = nums.length;
-    for(let i = 0; i<nums.length; i++){
-        for(let j = 0; j<n - i + 1; j++){
-            if(nums[j] > nums[j+1]){
-                [nums[j+1],nums[j]] = [nums[j],nums[j+1]];
-            }
-        }
-    }
+   for(let i = 1; i<nums.length;i++){
+       let temp = nums[i];
+       let j = i-1;
+       for(; j>=0 && nums[j] > temp; j--){
+           nums[j+1] = nums[j];
+       }
+       // [nu,nums[j+1]] = [nums[j+1],nums[i]];
+       nums[j+1] = temp;
+   }
 };
