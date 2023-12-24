fc289a073870aa80d5d810a2b5f22b774a6d94c4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-24 12:26:06 +0530

    Time: 44 ms (92.49%), Space: 42.4 MB (21.48%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,17 +2,13 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const findMinElement = (nums,start)=>{
-    let minId = start;
-    for(let i = start+1; i< nums.length; i++)
-        if(nums[minId] > nums[i])
-            minId = i;
-    return minId;
-}
 var sortColors = function(nums) {
+    
     for(let i = 0; i<nums.length; i++){
-        let minId = findMinElement(nums,i);
-        [nums[i], nums[minId]] = [nums[minId],nums[i]];
+        for(let j = i; j<nums.length; j++){
+            if(nums[i] > nums[j]){
+                [nums[i],nums[j]] = [nums[j],nums[i]];
+            }
+        }
     }
-    return nums;
 };
