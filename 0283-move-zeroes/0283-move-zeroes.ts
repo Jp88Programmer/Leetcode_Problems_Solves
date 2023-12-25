8f23965efa30977aaec3c17cfe1dda6f43cef096
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-25 23:08:12 +0530

    Time: 68 ms (94.39%), Space: 47.5 MB (46.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,13 +2,9 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    let start=0;
-    for(let i = 0; i<nums.length; i++){
-        if(nums[i]!==0){
-            let temp = nums[i];
-            nums[i] = nums[start];
-            nums[start] = temp;
-            start++;
-        }
+    let i = 0;
+    for(let n of nums){
+        if(n != 0) nums[i++] = n;
     }
+    while(i < nums.length) nums[i++] = 0;
 };
