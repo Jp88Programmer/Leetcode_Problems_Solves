e5ecc71e940a66efc2c0d1690f404c6d4bf21517
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-25 22:41:02 +0530

    Time: 49 ms (71.51%), Space: 41.9 MB (53.68%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -2,29 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var isSorted = function(nums){
-    
-    for(let i = 1; i<nums.length; i++){
-        if(nums[i-1] > nums[i])
-            return false;
-    }
-    return true;
-}
 var check = function(nums) {
+    let count = 0;
+    for(let i = 1; i<nums.length; i++)
+        if(nums[i-1] > nums[i]) count++;
     
-    if(isSorted(nums)) return true;
-    let par = 0;
-    for(let i = 0;i<nums.length-1; i++){
-        if(nums[i] > nums[i+1]){
-            par = i;
-            break;
-        }
-    }
-    let left1 = nums.slice(0,par+1);
-    let left2 = nums.slice(par+1,nums.length);
-    // console.log(left1,left2);
-    let arr = [...left2,...left1];
-    console.log(arr)
-    return isSorted(arr);
+    if(nums[nums.length-1] > nums[0]) count++;
+    return count <= 1;
     
 };
