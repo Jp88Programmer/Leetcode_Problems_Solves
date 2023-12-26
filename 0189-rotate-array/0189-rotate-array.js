553ab29ed8202e66dec2d97a6292e3516555607c
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2023-12-26 21:38:52 +0530

    Time: 82 ms (52.82%), Space: 50.8 MB (90.70%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,11 +4,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    
     let arr = [...nums];
-    let n = nums.length;
-    for(let i = 0; i<n; i++){
-        nums[(i+k)%n] = arr[i];
+    for(let i = 0; i<nums.length; i++){
+        nums[(i+k) % nums.length] = arr[i];
     }
-    
 };
