4d72f67c7bf7a76be38fe7bbf0de9b9a727ab3b0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-01-03 21:48:32 +0530

    Time: 57 ms (99.24%), Space: 49.6 MB (94.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,16 +4,14 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    
     let sum = 0;
     let map = new Map();
     map.set(sum,1);
     let maxSumArr = 0;
     nums.map(n => {
         sum += n;
-        if(map.has(sum - k)){
+        if(map.has(sum - k))
             maxSumArr+=map.get(sum-k);
-        }
         map.set(sum,map.has(sum)?map.get(sum)+1:1);
     })
     return maxSumArr;
