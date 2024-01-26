66a08f73443233998ec0dfd3a369e5585fd695f0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-01-26 23:06:22 +0530

    Time: 58 ms (39.89%), Space: 50.5 MB (10.11%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function(nums) {
+    
+    let ml = 0;
+    let dec = nums.map(n =>{
+        if(n.length>ml)
+            ml = n.length;
+        return parseInt(n,2);
+    });
+    let ans = new Array(17).fill(0);
+    dec.map(d => ans[d] = 1);
+    let str = "";
+    for(let i = 0; i<ans.length; i++){
+        if(ans[i] === 0){
+            str = i.toString(2);
+            break;
+        }
+    }
+    
+    if(ml>str.length){
+        return new Array(ml-str.length).fill('0').join("") + str;
+    }
+    return str;
+};
