85ef80aef340d1624950a599b54397f62bffd98a
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-01-26 22:37:31 +0530

    Time: 78 ms (73.82%), Space: 54.4 MB (11.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,9 +2,11 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    let i = 0;
-    for(let n of nums){
-        if(n != 0) nums[i++] = n;
-    }
-    while(i < nums.length) nums[i++] = 0;
+    let id = 0
+    nums.map((n,i) => {
+        if(n != 0)
+            nums[id++] = n;
+    })
+    while(id < nums.length)
+        nums[id++] = 0;
 };
