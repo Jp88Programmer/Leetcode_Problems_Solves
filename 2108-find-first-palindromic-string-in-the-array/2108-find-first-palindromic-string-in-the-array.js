1cc360743a1a4b47215153f7dfbe58e3be378252
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-02-13 23:09:22 +0530

    Time: 106 ms (11.66%), Space: 57.2 MB (13.89%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -2,15 +2,6 @@
  * @param {string[]} words
  * @return {string}
  */
-var isPal = function(str){
-    
-    for(let i = 0; i<str.length/2; i++){
-        
-        if(str.charAt(i) != str.charAt(str.length - i - 1))
-            return false;
-    }
-    return true;
-}
 var firstPalindrome = function(words) {
     
     for(let w of words)
