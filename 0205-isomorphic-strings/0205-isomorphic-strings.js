05d050b9b4b3e810568e7ffbab66cb898b5f2899
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-04-02 23:04:49 +0530

    Time: 2154 ms (5.01%), Space: 58.4 MB (5.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -11,31 +11,19 @@ var isIsomorphic = function(s, t) {
     for(let i = 0; i<s.length; i++){
         const ch1 = s.charAt(i);
         const ch2 = t.charAt(i);
-        if(alph1.hasOwnProperty(ch1)){
-            alph1[ch1].push(i);
-        }else{
-            alph1[ch1] = [i];
-        }
-        if(alph2.hasOwnProperty(ch2)){
-            alph2[ch2].push(i);
-        }else{
-            alph2[ch2] = [i];
-        }
+        alph1.hasOwnProperty(ch1) ?  alph1[ch1].push(i) : alph1[ch1] = [i]
+        alph2.hasOwnProperty(ch2) ?  alph2[ch2].push(i) : alph2[ch2] = [i]
     }
     
     for(let i = 0; i<t.length; i++){
         const ch = t.charAt(i);
         Object.entries(alph1).map(([key,value]) => {
-            // console.log(value)
             if(value.length === alph2[ch].length && JSON.stringify(value) === JSON.stringify(alph2[ch])){
                 alph1[key] = [];
                 alph2[ch] = []
             }
         })
     }
-    console.log(alph1,alph2);
-    if(JSON.stringify(Object.values(alph1)) == JSON.stringify(Object.values(alph2)))
-        return true;
-    else 
-        return false;
+    
+    return JSON.stringify(Object.values(alph1)) == JSON.stringify(Object.values(alph2));
 };
