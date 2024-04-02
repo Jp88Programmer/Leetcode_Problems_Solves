87ec3430f29bfb43bfb5f65797e2887679d2c299
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-04-02 23:08:14 +0530

    Time: 57 ms (76.50%), Space: 49.3 MB (96.69%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -5,25 +5,20 @@
  */
 var isIsomorphic = function(s, t) {
     
-    let alph1 = {};
-    let alph2 = {};
-    
-    for(let i = 0; i<s.length; i++){
-        const ch1 = s.charAt(i);
-        const ch2 = t.charAt(i);
-        alph1.hasOwnProperty(ch1) ?  alph1[ch1].push(i) : alph1[ch1] = [i]
-        alph2.hasOwnProperty(ch2) ?  alph2[ch2].push(i) : alph2[ch2] = [i]
+     // Base case: for different length of two strings...
+    if(s.length != t.length)
+        return false;
+    // Create two maps for s & t strings...
+    const map1 = [256];
+    const map2 = [256];
+    // Traverse all elements through the loop...
+    for(let idx = 0; idx < s.length; idx++){
+        // Compare the maps, if not equal, return false...
+        if(map1[s.charAt(idx)] != map2[t.charAt(idx)])
+            return false;
+        // Insert each character if string s and t into seperate map...
+        map1[s.charAt(idx)] = idx + 1;
+        map2[t.charAt(idx)] = idx + 1;
     }
-    
-    for(let i = 0; i<t.length; i++){
-        const ch = t.charAt(i);
-        Object.entries(alph1).map(([key,value]) => {
-            if(value.length === alph2[ch].length && JSON.stringify(value) === JSON.stringify(alph2[ch])){
-                alph1[key] = [];
-                alph2[ch] = []
-            }
-        })
-    }
-    
-    return JSON.stringify(Object.values(alph1)) == JSON.stringify(Object.values(alph2));
+    return true;    
 };
