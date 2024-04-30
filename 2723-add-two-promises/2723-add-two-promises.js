8d73b7e754d80f45cef87d97aa7af26cc6f97640
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-04-30 22:38:01 +0530

    Time: 71 ms (14.81%), Space: 49.5 MB (59.26%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -7,8 +7,8 @@ var addTwoPromises = async function(promise1, promise2) {
     
    const num1 = await promise1
    const num2 = await promise2
-   
-   return new Promise((resolve,reject) => resolve(num1 + num2))
+
+   return await num1+num2;
 };
 
 /**
