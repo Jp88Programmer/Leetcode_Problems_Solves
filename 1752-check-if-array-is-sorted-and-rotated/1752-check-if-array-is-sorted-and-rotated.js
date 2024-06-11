45c886c2c94cb6533750984dd9fd4a99ad103817
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-11 22:37:45 +0530

    Time: 55 ms (43.61%), Space: 49.1 MB (26.69%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var check = function(nums) {
+    
     let count = 0;
-    for(let i = 1; i<nums.length; i++)
-        if(nums[i-1] > nums[i]) count++;
-    
-    if(nums[nums.length-1] > nums[0]) count++;
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] > nums[(i+1) % nums.length])
+            count++;
+    }
     return count <= 1;
-    
 };
