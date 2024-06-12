4d1bf637977931162fbad101f505defc2f08db90
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-12 21:29:42 +0530

    Time: 64 ms (8.58%), Space: 49.2 MB (21.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,15 +2,32 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+//Insertion Sort 
 var sortColors = function(nums) {
-    let n = nums.length;
-   for(let i = 1; i<nums.length;i++){
-       let temp = nums[i];
-       let j = i-1;
-       for(; j>=0 && nums[j] > temp; j--){
-           nums[j+1] = nums[j];
-       }
-       // [nu,nums[j+1]] = [nums[j+1],nums[i]];
-       nums[j+1] = temp;
-   }
+   
+    let two = nums.length - 1;
+    let one = 0,i = 0;
+    
+    while(one <= two && i <= two){
+        if(nums[i] == 0){
+            [nums[i],nums[one]] = [nums[one],nums[i]];
+            i++;
+            one++;
+        }
+        else if(nums[i] == 2){
+            [nums[i],nums[two]] = [nums[two],nums[i]];
+            two--;
+        }
+        else if(nums[i] == 1) i++;
+        // console.log(nums)
+    }
+    
+    
+    
+    
+    // [1,2,1,0,0,2] 
+    // [1,2,1,0,0,2]
+    // [1,0,1,0,2,2]
+    // [0,0,1,1,2,2]
+    
 };
