a41f14da368b2de80e048d61449b20fdebe27846
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-14 23:35:41 +0530

    Time: 120 ms (59.45%), Space: 69.1 MB (50.39%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -3,21 +3,11 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
-        int steps = 0,new_value = 0;
-        for(int i = 0; i<nums.size(); i++){
-           steps += max(new_value-nums[i],0);
-            new_value = max(new_value,nums[i]) + 1;
+        int pre = nums[0],res = 0;
+        for(int i = 1; i<nums.size(); i++){
+           pre = max(pre + 1,nums[i]);
+           res += pre - nums[i];
         }
-        return steps;
+        return res;
     }
-         
-    //  sort(A.begin(), A.end());
-    //     int res = 0, need = 0;
-    //     for (int a: A) {
-    //         res += max(need - a, 0);
-    //         need = max(a, need)+1;
-    //     }
-    //     return res;
-    //                     r = 0,0,1,2,2,3,4
-    // [1,1,2,2,3,7]       n = 0,2,3,4,5,6,
 };
