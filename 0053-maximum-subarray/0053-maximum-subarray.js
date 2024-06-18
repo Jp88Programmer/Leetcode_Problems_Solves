154389daae183300f53d90d7947b0736775d16f4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-18 21:59:24 +0530

    Time: 81 ms (17.30%), Space: 59.1 MB (12.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
     let maxSum = Number.MIN_SAFE_INTEGER;
     let sum = 0;
-    for(let i = 0; i<nums.length; i++){
-        sum += nums[i];
-        maxSum = Math.max(sum,maxSum);
-        if(sum < 0) sum = 0;
-    }
-    return maxSum ;
+    
+    nums.map(num => {
+        sum += num;
+        maxSum = Math.max(maxSum,sum);
+        if(sum < 0)
+            sum = 0;
+    })
+    return maxSum;
 };
