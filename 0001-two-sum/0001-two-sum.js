da45e88470935e69287b7d2e985455414fc036b7
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-18 21:52:20 +0530

    Time: 46 ms (98.02%), Space: 51.2 MB (31.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,13 +6,13 @@
 var twoSum = function(nums, target) {
     
     let map = new Map();
-    
-    for(let i = 0; i<nums.length; i++){
-        let com = target - nums[i];
-        if(map.has(com)){
-            return [map.get(com),i];
+    let ans = [];
+    nums.map((num,index) => {
+        if(map.has(target - num)){
+            ans = [index,map.get(target-num)];
+        }else{
+            map.set(num,index);
         }
-        map.set(nums[i],i);
-    }
-    return [-1,-1];
+    })
+    return ans;
 };
