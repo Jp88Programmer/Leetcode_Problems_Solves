dd1cf06f8d7cd397ced96bdc03ce1f54631a1172
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-26 22:01:07 +0530

    Time: 77 ms (44.98%), Space: 59.5 MB (18.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,27 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+//     kandane's algorithm
+    // let ans = 0,sum = 0;
+    // for(let i = 1; i<prices.length; i++){
+    //    sum += prices[i] - prices[i-1];
+    //    sum = Math.max(0,sum);
+    //    ans = Math.max(ans,sum);
+    // }
+    // return ans;
     
-    let ans = 0,sum = 0;
-    for(let i = 1; i<prices.length; i++){
-       sum += prices[i] - prices[i-1];
-       sum = Math.max(0,sum);
-       ans = Math.max(ans,sum);
-    }
+//     calculate - curretn sell and overrall sell
+    
+    let lv = Number.MAX_SAFE_INTEGER,cs = 0,ans = 0;
+    
+    prices.map((p,i) => {
+        if(p < lv){
+            lv = p;
+        }
+        cs = p - lv;
+        
+        if(ans < cs)
+            ans = cs;
+    })
     return ans;
 };
