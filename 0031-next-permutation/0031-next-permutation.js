77e4f50435e22806515340adf5209f9a05120e97
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-28 23:08:07 +0530

    Time: 102 ms (5.49%), Space: 55.3 MB (5.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -2,32 +2,42 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+var binarySearch = function(nums,start,end,key){
+    while(start<=end){
+        let mid = Math.floor((start + end)/2);
+        if(nums[mid] > nums[key])
+            start = mid+1;
+        else
+            end = mid - 1;
+    }
+    return end;
+}
+var reverseArr = function(nums,start,end){
+    while(start<end){
+        [nums[start],nums[end]] = [nums[end],nums[start]]
+        start++,end--;
+    }
+}
 var nextPermutation = function(nums) {
     
-    let n = nums.length ;
-    let index = -1;
-    for(let i = n - 2; i >= 0; i--){
+    
+    let key = 0,flag = true;
+    for(let i = 0; i<nums.length-1; i++){
         if(nums[i] < nums[i+1]){
-            index = i;
-            break;
+            key = i;
+            flag = false;
         }
     }
-    // console.log(index)
-    if(index == -1)
-        nums.reverse();
-    else{
-        let id = index + 1;
-        for(let i = n - 1; i>=id; i--){
-            if(nums[i]>nums[index]){
-                [nums[i],nums[index]] = [nums[index],nums[i]];
-                break;
-            }
-        }
-        let arr = nums.slice(index+1,n);
-        arr.reverse();
-        // console.log(arr)
-        for(let i = id,k = 0; i<n && k < arr.length; i++){
-            nums[i] = arr[k++];
-        }
+    
+    if(flag){
+        nums.sort((a,b) => a - b);
+    }else{
+        let index = binarySearch(nums,key+1,nums.length-1,key);
+        console.log(index);
+        [nums[key],nums[index]] = [nums[index],nums[key]];
+        console.log(nums)
+        reverseArr(nums,key+1,nums.length-1);
+        console.log(nums)
     }
 };
