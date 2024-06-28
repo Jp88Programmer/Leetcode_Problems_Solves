fd52d3ac97c2920b5f4d0c608e1bc1a763c963a8
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-06-28 21:46:41 +0530

    Time: 48 ms (89.02%), Space: 49.5 MB (29.75%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,24 +3,10 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
-    
-    let buy = prices[0],pro = 0;
-    
-    for(let i = 0; i<prices.length; i++){
-        if(buy > prices[i]){
-            // prices[i] = buy;
-            buy = prices[i];
-        }
-        let cp = prices[i] - buy;
-        
-        if(cp > 0){
-            pro += cp;
-            buy = prices[i];
-        }
-        // console.log(pro)
-        
+
+    let ans = 0;
+    for(let i = 1; i<prices.length; i++){
+        ans += Math.max(0,prices[i] - prices[i-1]);
     }
-    
-    return pro;
+    return ans;
 };
