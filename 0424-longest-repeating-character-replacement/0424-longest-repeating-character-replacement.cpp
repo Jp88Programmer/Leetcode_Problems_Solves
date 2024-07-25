ade94e2878a6c98daaebc742ff4d917e68794fdb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-07-25 21:44:34 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (49.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        map<char,int>m;
-        int j = 0;
-        int res = 0;
-        int maxfr = 1;
+        int arr[26] = {0};
+        int maxCount = 0,j = 0;
         for(int i = 0; i<s.size(); i++){
-            m[s[i]]++;
-            maxfr = max(maxfr,m[s[i]]);
-            int len = i - j + 1;
-            if(len - maxfr <= k)
-                res = max(res,len);
-            
-            while(len - maxfr > k){
-                m[s[j]]--;
-                maxfr = max(maxfr,m[s[j]]);
-                j++;
-                len = i - j + 1;
+            maxCount = max(maxCount,++arr[s[i]-'A']);
+            if((i - j + 1) - maxCount > k){
+                arr[s[j++] - 'A']--;
             }
         }
-        return res;
+        return s.size() - j;
     }
 };
 
