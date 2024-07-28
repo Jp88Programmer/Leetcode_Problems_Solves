4448e6f3cb32e78bf58993337ed534842653e721
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-07-28 21:49:46 +0530

    Time: 62 ms (100.00%), Space: 51.3 MB (100.00%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canAliceWin = function(nums) {
+    
+    let sumDigitOne = 0,sumDigitTwo = 0;
+    
+    nums.map(num => {
+        if(Math.floor(num / 10) == 0){
+            sumDigitOne += num;
+        }else if(Math.floor(num / 100) == 0){
+            sumDigitTwo += num;
+        }
+    })
+    
+    if(sumDigitOne > sumDigitTwo){
+        return true;
+    }else if(sumDigitOne < sumDigitTwo){
+        return true;
+    }
+    return false;
+    
+};
