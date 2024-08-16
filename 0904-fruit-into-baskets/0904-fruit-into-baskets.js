57d37d0697d7877c2f27d8036f228947e419b225
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-08-16 11:23:34 +0530

    Time: 104 ms (62.04%), Space: 63.8 MB (34.10%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -4,23 +4,26 @@
  */
 var totalFruit = function(fruits) {
     
-    let map = new Map();
+    const map = new Map();
+    let ans = 0;
+    let sum = 0,j = 0;
     
-    let j = 0,i = 0,ans = 0;
-    for( i = 0; i<fruits.length; i++){
-        let fruit = fruits[i];
-        map.set(fruit,map.has(fruit)?map.get(fruit) + 1:1);
-        while(map.size > 2){
+    fruits.map((fruit,i) => {
+        
+        map.set(fruit,map.has(fruit) ? map.get(fruit) + 1 : 1);
+        // sum += map.get(fruit);
+        // console.log(map)
+        while(map.size > 2){    
             if(map.get(fruits[j]) == 1){
                 map.delete(fruits[j]);
             }else{
-                
-            map.set(fruits[j],map.get(fruits[j]) - 1);
+                map.set(fruits[j],map.get(fruits[j]) - 1);
             }
             j++;
         }
+        
         ans = Math.max(ans,i - j + 1);
-    }
-    return ans ;
+    })
+    return ans;
     
 };
