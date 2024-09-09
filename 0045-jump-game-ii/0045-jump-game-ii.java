3e2c49e98370ad2b88bbc1c730248699ef2f5129
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2024-09-09 23:00:27 +0530

    Time: 1 ms (98.08%), Space: 45.3 MB (26.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,13 +2,10 @@ class Solution {
     public int jump(int[] nums) {
         
         int reach = 0;
-        
         int i = 0,count = 0,end = 0;
-        
         for(i = 0; i<nums.length - 1 && i <= reach; i++){
             if(i + nums[i]>reach){
                 reach = i + nums[i];
-                // count++;
             }
             
             if(i == end){
