eb8874ff86bbeb5cfb1a5a87d205d0a896be2fad
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-01-04 22:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (53.23%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,7 +6,19 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m,n,...nums2);
-    // console.log(nums1);
-    nums1.sort((a,b) => a - b);
+    let n1 = m -1, n2 = n-1, r = m + n - 1;
+
+    while(n1 >= 0 && n2 >= 0){
+        if(nums1[n1] < nums2[n2]){
+            nums1[r--] = nums2[n2--];
+        }else{
+            nums1[r--] = nums1[n1--];
+        }
+    }
+
+    while(n2 >= 0 && r >= 0){
+        nums1[r--] = nums2[n2--];
+    }
+
+    
 };
