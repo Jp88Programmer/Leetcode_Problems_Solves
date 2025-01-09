acd73cc5c0a3ba968d6615604a7013fd568b6845
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-01-09 22:51:45 +0530

    Time: 1 ms (72.67%) | Memory: 51.4 MB (54.26%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+    if(nums.length == 1 && nums[0] != 0) return 0;
+    let sum = nums.reduce((acc,num) => acc + num,0);
+    let n = nums.length;
+    let sum1 = Math.floor( n * ( n + 1) / 2)
+    return sum1 - sum;
+    
+};
