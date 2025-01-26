f7b2c0e85fa68ef3e701607dcabaa8ceb7cefd80
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-01-26 19:37:17 +0530

    Time: 1 ms (100.00%) | Memory: 51.8 MB (100.00%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.js
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.js
@@ -4,7 +4,6 @@
  */
 var countPartitions = function(nums) {
     let prefixSum = [];
-
     let totalSum = nums.reduce((sum,num) => {
         sum += num;
         prefixSum.push(sum);
@@ -13,9 +12,7 @@ var countPartitions = function(nums) {
 
     let partition = 0;
     for(let i = 0; i<prefixSum.length - 1; i++){
-
         let diff = prefixSum[i] - (totalSum - prefixSum[i]);
-
         if(diff % 2 == 0){
             partition++;
         }
