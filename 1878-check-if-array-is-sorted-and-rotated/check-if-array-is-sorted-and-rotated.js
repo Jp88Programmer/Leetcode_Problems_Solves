6ea191f0739537b95aff758608c1beca95b70daa
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-02-02 19:10:53 +0530

    Time: 1 ms (21.69%) | Memory: 48.8 MB (70.13%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let count = 0;
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] > nums[(i+1) % nums.length])
+            count++;
+    }
+    return count <= 1;
+};
