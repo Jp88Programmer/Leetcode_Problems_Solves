1529b980b8c138d7dd435fc33bbb2c0b9dc08d4e
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-02-10 23:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.14%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+       int j = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(isdigit(s[i])){
+                j--;
+            }else{
+                s[j++] = s[i];
+            }
+        }
+        return s.substr(0,j);
+    }
+};
