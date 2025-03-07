3f1674b797379b2f59c8c88cd20df876731e01bf
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-07 22:36:52 +0530

    Time: 15 ms (6.72%) | Memory: 61.7 MB (5.33%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var coloredCells = function(n) {
+    
+    let dp = new Array(n+1).fill(0);
+    let oddNum = 3;
+    let presum = 0;
+    let sum = 1;
+    if(n == 1) return 1;
+    for(let i = 2; i<=n; i++){
+        presum += oddNum-2;
+        sum += oddNum;
+        oddNum+=2;
+        dp[i] = presum + sum ;
+    }
+    return dp[n];
+};
