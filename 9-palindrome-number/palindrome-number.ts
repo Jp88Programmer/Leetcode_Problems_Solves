190da51995b6889df00e85bd0aa4db0ab1bc04f5
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-09 16:24:34 +0530

    Time: 5 ms (86.34%) | Memory: 66 MB (8.21%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.ts
@@ -0,0 +1,12 @@
+function isPalindrome(x: number): boolean {
+    if(x < 0) 
+        return false;
+   let str = x.toString();
+    let s = 0, e = str.length -1;
+   while(s < e){
+     if(str.charAt(s) != str.charAt(e))
+        return false;
+    s++,e--;
+   }
+   return true;
+};
