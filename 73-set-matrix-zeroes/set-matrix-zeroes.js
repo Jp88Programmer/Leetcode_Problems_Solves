55eb7865099a68f31390d57d600859af4271944c
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-20 23:20:35 +0530

    Time: 6 ms (24.70%) | Memory: 59.6 MB (16.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,28 +2,23 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-
-const modRowCols = function(matrix,row,col) {
-        for(let i = 0; i<matrix.length; i++){
-            for(let j = 0; j<matrix[i].length; j++){
-                if(row == i || col == j)
-                    matrix[i][j] = 0;
-            }
-        }
-    }
 var setZeroes = function(matrix) {
     
-    let calrc = [];
-    
-    matrix.map((row,i)=>{
-        row.map((col,j)=>{
-            if(col == 0)
-                calrc.push([i,j]);
+    const cr = [];
+    const cc = [];
+    matrix.map((mat,row) => {
+        mat.map((m,col) => {
+            if(m == 0){
+                cr.push(row);
+                cc.push(col);
+            }
         })
     })
-    
-    calrc.map(arr =>{
-        modRowCols(matrix,arr[0],arr[1]);
-       
-    })
+
+    for(let i = 0; i<matrix.length; i++){
+        for(let j = 0; j<matrix[i].length; j++){
+            if(cr.includes(i) || cc.includes(j))
+                matrix[i][j] = 0;
+        }
+    }
 };
