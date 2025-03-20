2dad97a9ec53ee6041f7b6d81f0ad1e872d9eeff
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-20 23:07:05 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (26.32%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -3,27 +3,25 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    if(numRows == 1){
-        return [[1]];
-    }
-    if(numRows == 2){
-        return [[1],[1,1]];
-    }
 
-    let res = [[1],[1,1]];
+    if(numRows == 1) return [[1]];
+    if(numRows == 2) return [[1],[1,1]];
 
-    let itr = numRows - 2;
+    const res = new Array(numRows);
+    res[0] = [1];
+    res[1] = [1,1];
 
-    while(itr > 0){
-        let arr = [1];
-        let last = res[res.length-1];
-        for(let i = 0; i<last.length - 1; i++){
-            let sum = last[i] + last[i+1];
-            arr.push(sum);
+    for(let i = 2; i<numRows; i++){
+        const arr = new Array(i+1);
+        arr[0] = 1;
+        arr[i] = 1;
+        for(let j = 1; j<i; j++){
+            arr[j] = res[i-1][j-1] + res[i-1][j];
         }
-        arr.push(1);
-        res.push(arr);
-        itr--;
+        res[i] = arr;
     }
+
+    // console.log(res);
     return res;
+   
 };
