242db682182910ebb88e5d0a0a858b56337bb63c
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-22 12:02:55 +0530

    Time: 169 ms (80.98%) | Memory: 96.3 MB (40.56%) - LeetSync

--- 204-count-primes/count-primes.js
+++ 204-count-primes/count-primes.js
@@ -6,12 +6,18 @@ var countPrimes = function(n) {
     let res = new Array(n).fill(true);
     res[0] = res[1] = false;
 
+    let count = 0;
     for(let i = 2; i<=Math.sqrt(n); i++){
         if(res[i]){
+            count++;
             for(let j = i * i; j<n; j+=i)
                 res[j] = false;
         }
     }
 
-    return res.filter(r => r === true).length;
+    for(let i = Math.floor(Math.sqrt(n)+1); i<n; i++){
+        if(res[i]) count++;
+    }
+       
+    return count;
 };
