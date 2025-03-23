6ff1b66c383504111acd2583582227aea53cca4f
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-23 12:40:06 +0530

    Time: 14 ms (55.00%) | Memory: 62.6 MB (5.00%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.js
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.js
@@ -2,44 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-const primeFactor = function(num){
-
-    let arr = new Array(num+1).fill(true);
-    arr[0] = arr[1] = false;
-    for(let i = 2; i * i <= num; i++){
-        if(arr[i]){
-            for(let j = i * i; j<=num; j+=i){
-                arr[j] = false;
-            }  
-        }
-    }
-
-    
-
-    arr.map((a,i) => {
-        if(a && num % i != 0){
-            arr[i] = false;
-        }
-        if(arr[i]){
-            arr[i] = i;
-        }
-    });
-
-    // console.log(arr);
-    return arr.filter(a => typeof(a) === "number");
-}
 var distinctPrimeFactors = function(nums) {
-    
+    let pn =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
     let set = new Set();
-
     nums.map(num => {
-        let arr = primeFactor(num);
-        arr.map(a => set.add(a));
-        // set.add(...arr);
+
+        pn.map(p => {
+            if(num % p == 0){
+                set.add(p);
+                while(num % p == 0)
+                    num = Math.floor(num / p);
+            }
+        })
+
+        if(num != 1)
+            set.add(num);
     })
 
-    console.log(set)
-
-    return set.size;
-
+    return set.size
 };
