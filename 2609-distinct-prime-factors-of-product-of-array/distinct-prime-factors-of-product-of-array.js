0466b1f6e7d7b6bb0abc973dc75bc2cc93dd0ae5
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-23 12:43:22 +0530

    Time: 12 ms (60.00%) | Memory: 62.5 MB (5.00%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.js
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.js
@@ -6,7 +6,6 @@ var distinctPrimeFactors = function(nums) {
     let pn =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
     let set = new Set();
     nums.map(num => {
-
         pn.map(p => {
             if(num % p == 0){
                 set.add(p);
