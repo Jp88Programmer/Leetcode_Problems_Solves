3bdcb5a04f2f6b0904c366f4ce2c31668d53ad12
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-03-24 23:41:17 +0530

    Time: 39 ms (89.30%) | Memory: 53.5 MB (27.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -6,5 +6,15 @@ var fib = function(n) {
     
     if(n == 0) return 0;
     if(n == 1 || n == 2) return 1;
-    return fib(n-1) + fib(n-2);
+    
+    let arr = new Array(n+1);
+
+    arr[0] = 0;
+    arr[1] = arr[2] = 1;
+
+    for(let i = 3; i<=n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[n];
 };
