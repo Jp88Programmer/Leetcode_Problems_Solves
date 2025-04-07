cc497f4bcfa4f1a32fc2e910bf7dec28373cd8fc
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-07 22:57:37 +0530

    Time: 1 ms (89.16%) | Memory: 58.8 MB (24.07%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map = new Map();
+    for(let i = 0; i<nums.length; i++){
+        let com = target - nums[i];
+        if(map.has(com)){
+            return [map.get(com),i];
+        }
+        map.set(nums[i],i);
+    }
+    return [-1,-1];
+};
