02414ea0db4dba654636a96fce6e214b31c96ae4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-07 23:31:11 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (61.54%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+//Insertion Sort 
+var sortColors = function(nums) {
+        let s = 0,e = nums.length-1;
+        let i = 0;
+        while(s <= e && i <= e){
+            if(nums[i] == 0){
+                [nums[s],nums[i]] = [nums[i],nums[s]];
+                s++;
+                i++;
+            }
+            else if(nums[i] == 2){
+                [nums[i],nums[e]] = [nums[e],nums[i]];
+                e--;
+                
+            }
+            else if(nums[i] == 1)
+                i++;
+        }
+};
