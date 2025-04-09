92218c6d9fbdd53989c2986a93a56a5e21dac737
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-09 23:01:46 +0530

    Time: 13 ms (5.42%) | Memory: 65.7 MB (17.45%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -4,13 +4,13 @@
  */
 var maxProfit = function(prices) {
     
-    let buy = prices[0];
+    let buy = Number.MAX_SAFE_INTEGER;
     let sell = 0;
 
-    for(let i = 1; i<prices.length; i++){
-        buy = Math.min(buy,prices[i]);
-        sell = Math.max(sell,prices[i] - buy);
-    }
+    prices.map(p => {
+        buy = Math.min(buy,p);
+        sell = Math.max(sell,p - buy); 
+    });
 
     return sell;
 };
