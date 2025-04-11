f6b74c8af0330192a8d42a5f313982ae93bf8929
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-11 22:57:27 +0530

    Time: 36 ms (60.22%) | Memory: 57.8 MB (79.57%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countSymmetricIntegers = function(low, high) {
+    
+    let count = 0;
+    for(let i = low; i<=high; i++){
+        if(i.toString().length % 2 == 0){
+            let str = i.toString();
+            let sum = 0,sum1 = 0;
+            for(let j = 0; j < str.length/2; j++){
+                sum += parseInt(str.charAt(j));
+            }
+             for(let j = str.length/2; j < str.length; j++){
+                sum1 += parseInt(str.charAt(j));
+            }
+            if(sum == sum1)
+                count++;
+        }
+    }
+
+    return count;
+};
