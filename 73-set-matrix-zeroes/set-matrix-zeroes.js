fcacf596131054a46fe3093aa818bad92e76ab54
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-14 22:52:10 +0530

    Time: 6 ms (23.77%) | Memory: 59 MB (59.15%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -6,15 +6,12 @@ var setZeroes = function(matrix) {
     
     const cr = [];
     const cc = [];
-    const arr = [];
    
    for(let i = 0; i<matrix.length; i++){
         for(let j = 0; j<matrix[i].length; j++){
             if(matrix[i][j] == 0){
                 cr.push(i);
                 cc.push(j);
-                // matrix[0][j] = 0;
-                // matrix[i][0] = 0;
             }
         }
     }
@@ -26,19 +23,4 @@ var setZeroes = function(matrix) {
         }
     }
 
-    // for(let i = 0; i<matrix.length; i++){
-    //     if(matrix[i][0] == 0){
-    //         for(let j = 0; j<matrix[i].length; j++){
-    //             matrix[i][j] = 0;
-    //         }
-    //     }
-    // }
-
-    // for(let j = 0; j<matrix[0].length; j++){
-    //     if(matrix[0][j] == 0){
-    //         for(let i = 0; i<matrix.length; i++){
-    //             matrix[i][j] = 0;
-    //         }
-    //     }
-    // }
 };
