9f325a1010ef2dea6d558da290f3b0395f6a3695
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-15 22:39:56 +0530

    Time: 31 ms (74.29%) | Memory: 69.6 MB (23.94%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+    
+    const map = new Map();
+    let ans = 0;
+    let sum = 0,j = 0;
+    
+    fruits.map((fruit,i) => {
+        map.set(fruit,map.has(fruit) ? map.get(fruit) + 1 : 1);
+        while(map.size > 2){
+            map.set(fruits[j],map.get(fruits[j]) - 1);    
+            if(map.get(fruits[j]) == 0){
+                map.delete(fruits[j]);
+            }
+            j++;
+        }
+        ans = Math.max(ans,i - j + 1);
+    })
+    return ans;
+    
+};
