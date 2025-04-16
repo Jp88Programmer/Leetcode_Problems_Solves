4fdca4b1e13f0a533ba9f6163ea8637d38276a0d
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-16 23:06:01 +0530

    Time: 13 ms (49.21%) | Memory: 62.4 MB (39.68%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.ts
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.ts
@@ -1,16 +1,13 @@
 function numSubarraysWithSum(nums: number[], goal: number): number {
     
-    const map = new Map();
-
     let sum = 0,count = 0;
-
+    const map = new Map();
     map.set(sum,1);
-    
-    nums.map((num,i) => {
+
+    nums.map(num => {
         sum += num;
-        if(map.has(sum - goal)){
+        if(map.has(sum - goal))
             count += map.get(sum - goal);
-        }
         map.set(sum,map.has(sum) ? map.get(sum) + 1 : 1);
     })
 
