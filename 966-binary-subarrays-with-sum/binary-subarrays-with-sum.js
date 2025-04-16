8473a1d082733a9579834a49c4a0318d67aeaab4
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-16 22:57:19 +0530

    Time: 3 ms (66.27%) | Memory: 57.7 MB (48.82%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
@@ -7,22 +7,17 @@
 var fun = function(nums,goal) {
 
     if(goal < 0) return 0;
-    let count = 0;
-    let j = 0;
-    let sum = 0;
+
+    let count = 0,j = 0,sum = 0;
 
     for(let i = 0; i<nums.length; i++){
         sum += nums[i];
-        while(sum > goal){
+        while(sum > goal)
             sum -= nums[j++];
-        }
         count += (i - j + 1);
     }
-
     return count;
 }
 var numSubarraysWithSum = function(nums, goal) {
-
     return fun(nums,goal) - fun(nums,goal-1);
-  
 };
