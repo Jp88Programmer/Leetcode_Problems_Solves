0bcec3770bb7a764ec0d4103904eb5ad44b339bb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-17 23:12:43 +0530

    Time: 42 ms (9.06%) | Memory: 72.4 MB (5.00%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    
+    nums.map((num,i) => {
+        if(num % 2 == 0){
+            nums[i] = 0;
+        }else{
+            nums[i] = 1;
+        }
+    });
+
+    let count = 0,sum = 0;
+
+    const map = new Map();
+    map.set(sum,1);
+
+    for(let i = 0; i<nums.length; i++){
+        sum += nums[i];
+        if(map.has(sum - k)){
+            count += map.get(sum - k);
+        }
+        map.set(sum,map.has(sum)? map.get(sum) + 1 : 1);
+    }
+    
+    return count;
+};
