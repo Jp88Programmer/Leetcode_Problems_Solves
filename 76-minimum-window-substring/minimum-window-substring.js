4ab91f86af583823a90c154bec4aa8ff34ae9e27
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-10 11:32:09 +0530

    Time: 22 ms (78.03%) | Memory: 55 MB (93.92%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    
+    const map = new Map();
+    let si = -1,mlen = Number.MAX_SAFE_INTEGER,j = 0,count= 0;
+
+    for(let temp of t){
+        map.set(temp,map.has(temp) ? map.get(temp) + 1 : 1);
+    }
+
+    for(let i = 0; i<s.length; i++){
+
+        let st = s.charAt(i);
+        if(map.get(st) > 0) count++;
+        map.set(st,map.has(st) ? map.get(st) - 1: -1);
+
+        while(count == t.length){
+            let sj = s.charAt(j);
+            if((i-j+1) < mlen){
+                mlen = (i-j+1);
+                si = j;
+            }
+            map.set(sj,map.get(sj) + 1);
+            j++;
+            if(map.get(sj) > 0) count--;
+        }
+        
+    }
+
+    // console.log(si,mlen);
+    return si == -1 ? "" : s.substr(si,mlen);
+};
