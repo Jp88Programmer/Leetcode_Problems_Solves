658ceabe696b0a46ae6894f45ac2c5461242fef6
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-11 20:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (16.54%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.js
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -4,18 +4,18 @@
  */
 var threeConsecutiveOdds = function(arr) {
     
-    let count = [],res = 0;
+    let count = 0;
     for(let i = 0; i<arr.length; i++) {
         let a = arr[i];
-        if(count.length == 3){
-            res = true;
-            break;
+        if(a % 2 != 0){
+             if(count < 2){
+                count++;
+             }else if(count == 2){
+                return true;
+             }
+        }else{
+            count = 0;
         }
-        if(a % 2 == 0){
-            count = [];     
-        }
-        else
-            count.push(a);
     };
-    return count.length == 3;
+    return false;
 };
