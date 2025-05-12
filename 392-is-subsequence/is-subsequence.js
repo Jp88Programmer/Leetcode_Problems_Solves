95adca6e871400c04fbd354b1fdb96ee90e0fbfb
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-12 21:43:11 +0530

    Time: 3 ms (19.30%) | Memory: 55.9 MB (18.04%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,15 +4,10 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    
     if(t.length < s.length) return false;
-
     let j = 0;
-
     for(let i = 0; i<t.length; i++){
-        if(s.charAt(j) == t.charAt(i)){
-            j++;
-        }
+        if(s.charAt(j) == t.charAt(i)) j++;
     }
 
     return j == s.length;
