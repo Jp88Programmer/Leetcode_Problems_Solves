bc6bae7f8e74bdeab95cfb904994bf676f385dd3
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-13 23:00:53 +0530

    Time: 4 ms (98.28%) | Memory: 55.1 MB (96.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -12,7 +12,6 @@ var isAnagram = function(s, t) {
     for(let i = 0; i<s.length; i++){
         let si = s.charCodeAt(i);
         let ti = t.charCodeAt(i);
-        console.log(si,ti)
         arr[Number(si) - 97]++;
         arr[Number(ti) - 97]--;
     }
