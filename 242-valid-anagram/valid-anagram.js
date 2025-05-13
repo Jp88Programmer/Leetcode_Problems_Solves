c10c54cea62007f7cc358b2d7e6026a85b4fc9a9
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-13 23:00:41 +0530

    Time: 253 ms (5.11%) | Memory: 64.3 MB (5.03%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    if(s.length != t.length) return false;
+
+    let arr = new Array(26).fill(0);
+
+    for(let i = 0; i<s.length; i++){
+        let si = s.charCodeAt(i);
+        let ti = t.charCodeAt(i);
+        console.log(si,ti)
+        arr[Number(si) - 97]++;
+        arr[Number(ti) - 97]--;
+    }
+
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] != 0) return false;
+    }
+
+    return true;
+};
