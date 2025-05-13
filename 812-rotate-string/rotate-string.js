bbe172222710c2c3075d92357efb83b0e298b344
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-13 22:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (48.25%) - LeetSync

---
+++ 812-rotate-string/rotate-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    
+   return s.length == goal.length ? (s + s).includes(goal) : false;
+};
