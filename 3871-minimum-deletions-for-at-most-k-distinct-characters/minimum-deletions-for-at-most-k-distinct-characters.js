1e1e11ba4c9dd6fd0c297e1ee6fd25cfd73aef25
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-13 23:10:29 +0530

    Time: 4 ms (60.22%) | Memory: 56.3 MB (75.27%) - LeetSync

---
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var minDeletion = function(s, k) {
+    
+    let fr = {};
+    for(let i = 0; i<s.length; i++){
+        if(fr[s.charAt(i)]){
+            fr[s.charAt(i)]++;
+        }else{
+            fr[s.charAt(i)] = 1;
+        }
+    }
+
+    let arr = Object.entries(fr).sort((a,b) => a[1] - b[1]);
+
+    let rl = arr.length - k;
+
+    if(rl <= 0) return 0;
+
+    let op = 0;
+
+    for(let i = 0; i<rl; i++)
+        op += arr[i][1];
+
+    return op;
+
+};
