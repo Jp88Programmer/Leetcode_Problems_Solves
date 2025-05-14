4643b8b036db439bd98e993c9484c1ffc2ff51cc
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-14 08:31:07 +0530

    Time: 17 ms (57.25%) | Memory: 65 MB (34.35%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.ts
@@ -0,0 +1,26 @@
+function frequencySort(s: string): string {
+    let fr = {};
+
+    for(let i = 0; i<s.length; i++){
+        if(fr[s.charAt(i)]){
+            fr[s.charAt(i)]++;
+        }else{
+            fr[s.charAt(i)] = 1;
+        }
+    }
+
+    let arr = Object.entries(fr).sort((a:any,b:any) => b[1] - a[1]);
+
+    let res = "";
+
+    for(let i = 0; i<arr.length; i++){
+        let str = "";
+        let val = Number(arr[i][1]);
+        for(let j = 0; j < val; j++){
+            str += arr[i][0];
+        }
+        res += str;
+    }
+
+    return res;
+};
