deefbbd3195b5c6c2f653d446da235a9cf854931
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-16 22:24:48 +0530

    Time: 19 ms (42.49%) | Memory: 58.9 MB (24.66%) - LeetSync

---
+++ 18-4sum/4sum.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+
+var ThreeSum = function(nums,start,end,target,ans){
+    let num1 = nums[start];
+    for(let i = start+1; i <= end; i++){
+        let num2 = nums[i];
+        let sum = target - num2;
+        let left = i+1,right = end;
+        if(i == start+1 || nums[i] != nums[i-1]){
+            while(left < right){
+                if(nums[left] + nums[right] < sum)
+                    left++;
+                else if(nums[left] + nums[right] > sum)
+                    right--;
+                else{
+                    let arr = [num1,num2,nums[left],nums[right]];
+                    ans.push(arr);
+                    
+                    while(left < right && nums[left] == nums[left+1])
+                        left++;
+                    while(left < right && nums[right] == nums[right-1])
+                        right--;
+                    
+                    left++;
+                    right--;
+                }
+            }
+        }
+    }
+}
+var fourSum = function(nums, target) {
+    let ans = []
+    nums.sort((a,b) => a - b);
+    for(let i = 0; i<nums.length; i++){
+        if(i == 0 || nums[i] != nums[i-1])
+            ThreeSum(nums,i,nums.length-1,target-nums[i],ans);
+    }
+    return ans;
+};
