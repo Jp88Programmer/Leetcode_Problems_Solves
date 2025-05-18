02f263deb3de345d5dd735ca33affa4fe0e42202
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-18 21:35:06 +0530

    Time: 1 ms (70.37%) | Memory: 57.8 MB (100.00%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.ts
@@ -0,0 +1,15 @@
+function smallestIndex(nums: number[]): number {
+    let index = -1;
+    let arr = [...nums];
+
+    for(let i = 0 ; i<nums.length; i++){
+        arr[i] = nums[i].toString().split("").reduce((acc,val) => acc + Number(val),0);
+
+        if(arr[i] == i){
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+};
