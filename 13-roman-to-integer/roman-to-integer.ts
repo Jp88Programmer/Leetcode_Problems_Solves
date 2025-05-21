706192f53c8046369a1431d784216518d75b5b75
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-21 21:41:58 +0530

    Time: 9 ms (25.43%) | Memory: 61.7 MB (65.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -1,12 +1,12 @@
 function romanToInt(s: string): number {
     const rn = {
-        I  : 1,
-        V  : 5,
-        X  : 10,
-        L  : 50,
-        C  : 100,
-        D  : 500,
-        M  : 1000,
+        "I"  : 1,
+        "V"  : 5,
+        "X"  : 10,
+        "L"  : 50,
+        "C"  : 100,
+        "D"  : 500,
+        "M"  : 1000,
     }
 
     let res = 0;
