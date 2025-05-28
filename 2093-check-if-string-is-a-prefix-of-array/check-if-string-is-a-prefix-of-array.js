70af993c9a01c9973ef62ddb090ace3d14faa4ae
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-05-28 21:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (23.64%) - LeetSync

--- 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.js
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.js
@@ -6,14 +6,12 @@
 var isPrefixString = function(s, words) {
     
     let com = "",res = "";
-
     for(let i = 0; i<words.length; i++){
         let word = words[i];
         if(s.includes(word) && s.includes(com + word)){
             res += word;
         }
         com += word;
-    }
-        
+    } 
     return s == res;
 };
