a4308cdb2e54116c3eb148ab6298487942abd399
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-06-18 22:07:00 +0530

    Time: 111 ms (14.81%) | Memory: 92.1 MB (7.41%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    nums.sort((a,b) => a - b);
+    let ans = [];
+    let id = 0;
+    while(id < nums.length){
+        let diff = nums[id] + k;
+        let temp = [];
+        let i = id;
+        for( ; i<nums.length && i < id + 3; i++){
+            if(nums[i] <= diff){
+                temp.push(nums[i]);
+            }
+        }
+        id = i;
+        if(temp.length < 3){
+            return [];
+        }
+        ans.push(temp);
+    }
+    return ans;
+};
