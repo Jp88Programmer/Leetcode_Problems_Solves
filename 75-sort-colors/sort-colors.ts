695fea0357790e07a144d67f1564030575737287
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-07-07 22:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (34.51%) - LeetSync

---
+++ 75-sort-colors/sort-colors.ts
@@ -0,0 +1,22 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    let s = 0, e = nums.length-1;
+    let i = 0;
+    while(i <= e){
+         if(nums[i]==0){
+            let temp = nums[i];
+            nums[i++] = nums[s];
+            nums[s] = temp;
+            s++;
+        }
+        else if(nums[i]==2){
+            let temp = nums[i];
+            nums[i] = nums[e];
+            nums[e] = temp;
+            e--;
+        }else
+            i++;
+    }
+};
