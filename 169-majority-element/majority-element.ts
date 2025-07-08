2d924ef8a027da6276bae67e70ae7147f17501a0
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-07-08 22:29:48 +0530

    Time: 1 ms (88.55%) | Memory: 57.8 MB (76.19%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,19 @@
+function majorityElement(nums: number[]): number {
+    let count = 1;
+    let ans = nums[0];
+
+    for(let i = 1; i<nums.length; i++) {
+        const num = nums[i];
+        if(count == 0){
+            ans = num;
+            count++;
+        }else if(ans == num){
+            count++;
+        }else{
+            count--;
+        }
+
+    }
+
+    return ans;
+};
