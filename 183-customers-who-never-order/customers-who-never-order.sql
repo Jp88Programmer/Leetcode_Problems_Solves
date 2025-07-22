8d14239dc36d6f573a750094aa6ddf113564036b
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-07-22 23:56:25 +0530

    Time: 622 ms (21.15%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,4 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 SELECT c.name AS Customers FROM Customers AS c
 LEFT JOIN Orders AS o ON c.id = o.customerId
 WHERE o.id IS NULL;
