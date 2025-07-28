b49970e5a27f206cc1fb57133f3febafd283be4a
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-07-28 23:35:58 +0530

    Time: 374 ms (91.01%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,6 @@
--- Write your PostgreSQL query statement below
-SELECT w1.id FROM Weather AS w1
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather AS w1
 JOIN Weather AS w2
-ON w1.recordDate - (INTERVAL '1 days') = w2.recordDate
+  ON DATE_SUB(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
 WHERE w1.temperature > w2.temperature;
