cb1fa39ce7142309b008dffd84a11fd3d7202fe9
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-08-15 22:43:27 +0530

    Time: 1 ms (23.87%) | Memory: 56.9 MB (63.51%) - LeetSync

--- 342-power-of-four/power-of-four.js
+++ 342-power-of-four/power-of-four.js
@@ -2,6 +2,8 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-    return Number.isInteger(Math.log(n)/Math.log(4));
+var isPowerOfFour = function(num,addBit = 0) {
+    if(addBit == 32) return false;
+    if(num == 1 << addBit) return true;
+    return isPowerOfFour(num,addBit + 2);
 };
