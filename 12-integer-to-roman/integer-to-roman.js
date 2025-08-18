76877083ce8e34f2a6963048f6bb6b97191d3c05
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-08-18 21:40:09 +0530

    Time: 5 ms (65.21%) | Memory: 61.9 MB (55.22%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+        let ans = "";
+        let vecc = []; 
+        vecc.push([1000,"M"]);
+        vecc.push([900,"CM"]);
+        vecc.push([500,"D"]);
+        vecc.push([400,"CD"]);
+        vecc.push([100,"C"]);
+        vecc.push([90,"XC"]);
+        vecc.push([50,"L"]);
+        vecc.push([40,"XL"]);
+        vecc.push([10,"X"]);
+        vecc.push([9,"IX"]);
+        vecc.push([5,"V"]);
+        vecc.push([4,"IV"]);
+        vecc.push([1,"I"]);
+        
+        for(let i=0; i<13; i++){
+            while(num>=vecc[i][0])
+            {
+                num -= vecc[i][0];
+                ans += vecc[i][1];
+            }
+        }
+        
+        return ans;
+};
