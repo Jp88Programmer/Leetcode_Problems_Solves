403a792660984f5a8c79e6cd105be9982a0d3ad1
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-08-23 23:25:34 +0530

    Time: 3 ms (86.23%) | Memory: 59.8 MB (33.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,17 +6,14 @@ function isPal(str){
     
     let s = 0,e = str?.length -1;
     while(s < e){
-        if(str.charAt(s++) != str.charAt(e--))
+        if(str.toLowerCase().charAt(s++) != str.toLowerCase().charAt(e--))
             return false;
     }
     return true;
 
 }
 var isPalindrome = function(s) {
-    
     let reg = /[a-z|A-Z|0-9]/g;
     let ans = s.match(reg);
-    ans?.map((a,i) =>{ ans[i] = a.toLowerCase()});
-    console.log(ans?.join(""))
     return isPal(ans?.join(""));
 };
