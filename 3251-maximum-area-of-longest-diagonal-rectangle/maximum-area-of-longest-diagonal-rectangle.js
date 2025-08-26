55c6c579bccdd2fbdba679fabedb49d37ae5dbb9
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-08-27 00:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 56.7 MB (40.00%) - LeetSync

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} dimensions
+ * @return {number}
+ */
+var areaOfMaxDiagonal = function(dimensions) {
+    
+    let maxdi = Number.MIN_SAFE_INTEGER;
+    let area = 0;
+    dimensions.map((d,i) => {
+        let di = d[0] * d[0] + d[1] * d[1];
+        if(maxdi < di || (maxdi == di && area < d[0] * d[1])){
+            maxdi = di;
+            area = d[0] * d[1];
+        }
+    })
+
+    return area;
+};
