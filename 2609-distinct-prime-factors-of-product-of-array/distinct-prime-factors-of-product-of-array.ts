ac118a26c653192372f93b37d65feeb3d3928454
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-08-27 22:15:50 +0530

    Time: 210 ms (9.09%) | Memory: 63.5 MB (9.09%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.ts
@@ -0,0 +1,42 @@
+function givePrimeNumbers(num){
+    let arr = new Array(num+1).fill(1);
+    arr[0] = arr[1] = 0;
+    for(let i = 2; i * i <= num; i++){
+        if(arr[i] == 1){
+            for(let j = i * i; j <= num; j += i){
+                if(arr[j] == 1)
+                    arr[j] = 0;
+            }
+        }
+    }
+
+    return arr.reduce((a,e,i) => {
+        if(e == 1){
+            a.push(i);
+        }
+        return a;
+    },[])
+
+}
+
+function distinctPrimeFactors(nums: number[]): number {
+    
+    let maxNum = Math.max(...nums);
+
+    let primeArr = givePrimeNumbers(maxNum);
+
+    // console.log(primeArr);
+
+    let set = new Set();
+    primeArr.map((p,i) => {
+        nums.map(n => {
+            if((p == n || (p <= n/2)) && n % p == 0){
+                set.add(p);
+            }
+        })
+    });
+
+    // console.log(set,set.size)
+
+    return set.size; 
+};
