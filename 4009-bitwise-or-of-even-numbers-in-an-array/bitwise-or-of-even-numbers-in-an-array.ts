6f16872f1e037baa2928f19f409ad7c8c6c20adc
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-09-21 23:16:00 +0530

    Time: 1 ms (100.00%) | Memory: 57.5 MB (100.00%) - LeetSync

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.ts
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.ts
@@ -1,11 +1,3 @@
 function evenNumberBitwiseORs(nums: number[]): number {
-    
-    let bitOr = 0;
-
-    nums.map(num => {
-        if(num % 2 == 0){
-            bitOr |= num;
-        }
-    })
-    return bitOr;
+    return nums.reduce((bitOr,num) => num % 2 == 0 ? bitOr | num : bitOr,0);
 };
