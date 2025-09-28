b6d752b975d903d1a80a9cec961bc7b0761719b9
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-09-28 23:27:21 +0530

    Time: 6 ms (4.03%) | Memory: 58.5 MB (18.55%) - LeetSync

---
+++ 4039-compute-decimal-representation/compute-decimal-representation.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var decimalRepresentation = function(n) {
+    let num = n.toString();
+    let res = [];
+    let mul10 = 1;
+    for(let i = num.length - 1; i>=0; i--){
+        let val = parseInt(num.charAt(i)) * mul10;
+        if(val > 0){
+            res.push(val);
+        }
+        mul10 *= 10;
+    }
+    res.reverse()
+    return res;
+};
