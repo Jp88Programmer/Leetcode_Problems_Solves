0b7193ede5dff53304db13b2cd7845ca31d916ab
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2026-07-29 00:08:45 +0530

    Time: 4 ms (53.39%) | Memory: 59.6 MB (5.22%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,35 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let left = 0, right = nums.length-1; 
-    let ans = [];
-    let map = new Map();
-    nums.map((n,i) => {
-        if(map.has(n)){
-            if(Array.isArray(map.get(n))){
-                let arr = map.get(n);
-                arr.push(i);
-                map.set(n, arr);
-            }else{
-                let val = map.get(n);
-                map.set(n, [val,i]);
-            }
-        }else{
-            map.set(n, i);
-        }
-    });
-
-    for(let i = 0; i < nums.length; i++){
-        let key = target - nums[i]
+   let ans = [];
+   const map = new Map();
+   for(let [i,num] of nums.entries()){
+        let key = target - num;
         if(map.has(key)){
-            let val = map.get(key);
-            if(Array.isArray(val) && val.filter(v => v != i).length > 0){
-                return [i,val.filter(v => v != i)[0]];
-            }else if(i !== val){
-                return [i,map.get(key)];
-            }
+            ans.push(i);
+            ans.push(map.get(key));
+            return ans;
+        }else{
+            map.set(num,i);
         }
-    }
-
-    return [];
+   }
+    return [-1,-1];
 };
